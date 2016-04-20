7a7ec9698f09080de5af1eec2533169b6f6e4d21
Mahmoud Zalt <mahmoud.z@msn.com>
2016-04-20 17:35:20 +0300

    reset the Exception Handler (return it to it's original state)

--- server/app/Exceptions/Handler.php
+++ server/app/Exceptions/Handler.php
@@ -3,9 +3,10 @@
 namespace Mega\Exceptions;
 
 use Exception;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -16,8 +17,10 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
+        AuthorizationException::class,
         HttpException::class,
         ModelNotFoundException::class,
+        ValidationException::class,
     ];
 
     /**
@@ -30,7 +33,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $e)
     {
-        return parent::report($e);
+        parent::report($e);
     }
 
     /**
@@ -42,15 +45,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if ($e instanceof ModelNotFoundException) {
-            $e = new NotFoundHttpException($e->getMessage(), $e);
-        }
-
-        // TODO: clear this
-        if ($e instanceof NotFoundHttpException) {
-           dd('Wait a second! ' . $request->url() . ' does not exist!!');
-        }
-
         return parent::render($request, $e);
     }
 }
