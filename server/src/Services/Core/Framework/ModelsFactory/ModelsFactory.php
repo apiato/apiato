81630e3e37b1a63e0eea74a9f9499be71c6dac04
Mahmoud Zalt <mahmoud.z@msn.com>
2016-04-21 13:38:40 +0300

    remove testing models factories

--- server/src/Services/Core/Framework/ModelsFactory/ModelsFactory.php
+++ server/src/Services/Core/Framework/ModelsFactory/ModelsFactory.php
@@ -8,23 +8,4 @@ $factory->define(Mega\Modules\User\Models\User::class, function (Faker\Generator
     ];
 });
 
-// Account
-$factory->define(Mega\Modules\Account\Models\Account::class, function (Faker\Generator $faker) {
-    return [
-        'user_id'  => factory(Mega\Modules\User\Models\User::class)->create()->id,
-        'url'      => $faker->url,
-        'username' => $faker->userName,
-        'secret'   => $faker->password(),
-        'note'     => $faker->text(100),
-    ];
-});
-
-// Tag
-$factory->define(Mega\Modules\Tag\Models\Tag::class, function (Faker\Generator $faker) {
-    return [
-        'user_id' => factory(Mega\Modules\User\Models\User::class)->create()->id,
-        'label'   => $faker->unique()->streetName,
-    ];
-});
-
 // ...
