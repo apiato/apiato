bda7c71beef28f49e54dc425b9651d23c29f0bc0
Mahmoud Zalt <mahmoud.z@msn.com>
2016-04-21 13:31:52 +0300

    create delete user controller

---
+++ server/src/Modules/User/Controllers/Api/DeleteUserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mega\Modules\User\Controllers\Api;
+
+use Mega\Modules\User\Requests\DeleteUserRequest;
+use Mega\Modules\User\Tasks\DeleteUserTask;
+use Mega\Services\Core\Controller\Abstracts\ApiController;
+
+/**
+ * Class DeleteUserController
+ *
+ * @type     Controller
+ * @package  Mega\Interfaces\Api\Controllers
+ * @author   Mahmoud Zalt <mahmoud@zalt.me>
+ */
+class DeleteUserController extends ApiController
+{
+
+    /**
+     * @param \Mega\Modules\User\Requests\DeleteUserRequest $deleteUserRequest
+     * @param \Mega\Modules\User\Tasks\DeleteUserTask       $deleteUserTask
+     * @param                                               $userId
+     *
+     * @return \Dingo\Api\Http\Response
+     */
+    public function handle(DeleteUserRequest $deleteUserRequest, DeleteUserTask $deleteUserTask, $userId)
+    {
+        $deleteUserTask->run($userId);
+
+        return $this->response->accepted(null, [
+            'message' => 'User (' . $userId . ') Deleted Successfully.'
+        ]);
+
+    }
+
+}
