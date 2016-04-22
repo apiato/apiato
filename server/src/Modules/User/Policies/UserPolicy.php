bb855b2a265523f50b2bac374e0d64770f4d88be
Mahmoud Zalt <mahmoud.z@msn.com>
2016-04-22 14:09:53 +0300

    create User Policy

---
+++ server/src/Modules/User/Policies/UserPolicy.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Mega\Modules\User\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Mega\Modules\User\Models\User;
+
+/**
+ * Class UserPolicy
+ *
+ * @author Mahmoud Zalt <mahmoud@zalt.me>
+ */
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine if the user is updating himself and not another user.
+     *
+     * @param \Mega\Modules\User\Models\User $user
+     * @param                                $inputUserId
+     *
+     * @return bool
+     */
+    public function update(User $user, $inputUserId)
+    {
+        // authorize only if a user is updating it's own records
+        return ($user->id == $inputUserId) ? true : false;
+    }
+
+    /**
+     * Determine if the user is deleting himself and not another user.
+     *
+     * @param \Mega\Modules\User\Models\User $user
+     * @param                                $inputUserId
+     *
+     * @return bool
+     */
+    public function delete(User $user, $inputUserId)
+    {
+        // authorize only if a user is deleting it's own records
+        return ($user->id == $inputUserId) ? true : false;
+    }
+
+}
