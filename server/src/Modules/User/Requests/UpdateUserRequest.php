6d850e9b7950c1b37e4ae4d24b6d70564e667d5e
Mahmoud Zalt <mahmoud.z@msn.com>
2016-04-22 14:26:46 +0300

    replace the policy helper with  a Gate instance

--- server/src/Modules/User/Requests/UpdateUserRequest.php
+++ server/src/Modules/User/Requests/UpdateUserRequest.php
@@ -2,6 +2,8 @@
 
 namespace Mega\Modules\User\Requests;
 
+use Illuminate\Contracts\Auth\Access\Gate;
+use Mega\Modules\User\Models\User;
 use Mega\Services\Core\Request\Abstracts\Request;
 
 /**
@@ -25,14 +27,17 @@ class UpdateUserRequest extends Request
     }
 
     /**
+     *
      * Determine if the user is authorized to make this request.
+     *
+     * @param \Illuminate\Contracts\Auth\Access\Gate $gate
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(Gate $gate)
     {
-        // $this->user() is the current logged in user, taken from the request
-        // $this->id is the request input user ID (for the user that needs to be updated)
-        return policy($this->user())->update($this->user(), $this->id);
+        // $this->user(): is the current logged in user, taken from the request
+        // $this->id: is the request input user ID (for the user that needs to be updated)
+        return $gate->getPolicyFor(User::class)->update($this->user(), $this->id);
     }
 }
