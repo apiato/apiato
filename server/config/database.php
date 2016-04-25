d887d5590333a9f589ff1f5a243c9066c8b659e5
Mahmoud Zalt <mahmoud.z@msn.com>
2016-04-25 18:19:22 +0300

    add redis config variables to the env file

--- server/config/database.php
+++ server/config/database.php
@@ -133,12 +133,12 @@ return [
 
     'redis' => [
 
-        'cluster' => false,
+        'cluster' => env('REDIS_CLUSTER', false),
 
         'default' => [
-            'host'     => '127.0.0.1',
-            'port'     => 6379,
-            'database' => 0,
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DATABASE', 0),
         ],
 
     ],
