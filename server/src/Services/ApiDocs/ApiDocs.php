0c5b3379ba7d7d089ac11dcb42c7f9a65d7b0742
Troy Carefoot <Troy Carefoot>
2016-04-24 23:38:46 -0700

    Added more asterisks to match the style in the other route file.

--- server/src/Services/ApiDocs/ApiDocs.php
+++ server/src/Services/ApiDocs/ApiDocs.php
@@ -2,12 +2,12 @@
 
 //====[ FOR API DOC JS ]====\\
 
-/**
+/*********************************************************************************
  * @apiDefine Headers_Normal
  * @apiHeader Accept application/json
  */
 
-/**
+/*********************************************************************************
  * @apiDefine Headers_Authenticated
  * @apiHeader Accept application/json
  * @apiHeader Authorization Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ91QiLCJhbGciOiJIUzI1NiJ1...
