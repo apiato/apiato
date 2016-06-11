3bd7b87d2c4fa4ee73061f23f8bc5d239358d569
Mahmoud Zalt <mahmoud.z@msn.com>
2016-06-11 05:28:46 +0300

    add the module.php config file with proper naming

---
+++ config/modules.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'modules' => [
+        'namespace' => 'Hello',
+
+        'register' => [
+            'User' => [
+                'routes' => [
+                    'api' => [
+                        ['fileName' => 'v1', 'versionNumber' => '1']
+                    ],
+                    'web' => [
+                        ['fileName' => 'main']
+                    ]
+                ]
+            ],
+        ],
+
+    ],
+];
