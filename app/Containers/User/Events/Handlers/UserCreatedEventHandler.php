544070d728014cf13892f0b6a4b6bcb8002b68f5
Mahmoud Zalt <mahmoud.z@msn.com>
2016-06-27 18:07:38 +0300

    use the mail service on the user create service

--- app/Containers/User/Events/Handlers/UserCreatedEventHandler.php
+++ app/Containers/User/Events/Handlers/UserCreatedEventHandler.php
@@ -3,6 +3,7 @@
 namespace App\Containers\User\Events\Handlers;
 
 use App\Containers\User\Events\Events\UserCreatedEvent;
+use App\Services\Mails\Mails\WelcomeEmail;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -13,22 +14,17 @@ use Illuminate\Contracts\Queue\ShouldQueue;
  */
 class UserCreatedEventHandler implements ShouldQueue
 {
-
-    /**
-     * Create the event handler.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
     /**
      * @param \App\Containers\User\Events\Events\UserCreatedEvent $event
      */
     public function handle(UserCreatedEvent $event)
     {
         dump('New User Created Successfully!');
+
+        $email = new WelcomeEmail();
+        $email->setEmail('testing@tester.com');
+        $email->setName('Mahmoud Zalt');
+        $email->send($data = []);
+
     }
 }
