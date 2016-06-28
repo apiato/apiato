15e93a1ac57d169d6d6673826a7c4e9104f07508
Mahmoud Zalt <mahmoud.z@msn.com>
2016-06-28 07:15:49 +0300

    switch email confirmation route from api to web

--- app/Containers/Email/Routes/Api/v1.php
+++ app/Containers/Email/Routes/Api/v1.php
@@ -17,16 +17,3 @@ $router->post('/users/{id}/email', [
         'api.auth',
     ],
 ]);
-
-/*********************************************************************************
- * @apiGroup           Emails
- * @apiName            ConfirmUserEmailController
- * @api                {get} /users/{id}/email/confirmation/{code} you do not have to touch this endpoint
- * @apiDescription     confirm the email address (the user will receive an email with this endpoint to confirm his email)
- * @apiVersion         1.0.0
- * @apiPermission      none
- * @apiHeader          Accept application/json
- */
-$router->get('/users/{id}/email/confirmation/{code}', [
-    'uses' => 'ConfirmUserEmailController@handle',
-]);
