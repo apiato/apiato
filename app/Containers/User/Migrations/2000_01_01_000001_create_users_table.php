c968b815182071fa44eafef7558722f126107517
Mahmoud Zalt <mahmoud.z@msn.com>
2016-06-28 06:24:36 +0300

    let the confirmed user field be false by default

--- app/Containers/User/Migrations/2000_01_01_000001_create_users_table.php
+++ app/Containers/User/Migrations/2000_01_01_000001_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
 
             $table->string('email')->unique();
-            $table->boolean('confirmed');
+            $table->boolean('confirmed')->default(false);
 
             $table->rememberToken();
             $table->timestamps();
