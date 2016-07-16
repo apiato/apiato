e07b9e94cb27b7e21c6f3172e1e753e94dcc7683
Mahmoud Zalt <mahmoud.z@msn.com>
2016-07-16 05:23:35 +0300

    upgrade to laravel 5.2: remove outdates service providers

--- config/app.php
+++ config/app.php
@@ -131,13 +131,11 @@ return [
         /*
          * Laravel Framework
          */
-        Illuminate\Foundation\Providers\ArtisanServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Routing\ControllerServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
