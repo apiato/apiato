6bb3475b3a5aa545dc4968624e8ce3159f479d6c
Mahmoud Zalt <mahmoud.z@msn.com>
2016-07-19 20:10:22 +0300

    S: create CreateUserService to be used by many actions

---
+++ app/Containers/User/Services/CreateUserService.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Containers\User\Services;
+
+use App\Containers\ApiAuthentication\Services\ApiAuthenticationService;
+use App\Containers\User\Contracts\UserRepositoryInterface;
+use App\Containers\User\Exceptions\AccountFailedException;
+use App\Port\Service\Abstracts\Service;
+use Exception;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * Class CreateUserService.
+ *
+ * @author Mahmoud Zalt <mahmoud@zalt.me>
+ */
+class CreateUserService extends Service
+{
+
+
+    /**
+     * @var \App\Containers\User\Contracts\UserRepositoryInterface
+     */
+    private $userRepository;
+
+    /**
+     * @var \App\Containers\ApiAuthentication\Services\ApiAuthenticationService
+     */
+    private $authenticationService;
+
+    /**
+     * CreateUserService constructor.
+     *
+     * @param \App\Containers\User\Contracts\UserRepositoryInterface              $userRepository
+     * @param \App\Containers\ApiAuthentication\Services\ApiAuthenticationService $authenticationService
+     */
+    public function __construct(
+        UserRepositoryInterface $userRepository,
+        ApiAuthenticationService $authenticationService
+    ) {
+        $this->userRepository = $userRepository;
+        $this->authenticationService = $authenticationService;
+    }
+
+
+    /**
+     * @param            $email
+     * @param            $password
+     * @param            $name
+     * @param bool|false $login
+     *
+     * @return  mixed
+     */
+    public function run($email, $password, $name, $login = false)
+    {
+        $hashedPassword = Hash::make($password);
+
+        try {
+            // create new user
+            $user = $this->userRepository->create([
+                'email'    => $email,
+                'password' => $hashedPassword,
+                'name'     => $name,
+            ]);
+        } catch (Exception $e) {
+            throw (new AccountFailedException())->debug($e);
+        }
+
+        if ($login) {
+            // login this user using it's object and inject it's token on it
+            $user = $this->authenticationService->loginFromObject($user);
+        }
+
+        return $user;
+    }
+
+
+}
