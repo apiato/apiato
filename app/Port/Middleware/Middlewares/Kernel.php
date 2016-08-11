9ef58fb2ff2b72854703cea87c06c7c58b387423
Mahmoud Zalt <mahmoud.z@msn.com>
2016-08-11 20:33:25 +0300

    S: set the web.auth Middleware

--- app/Port/Middleware/Middlewares/Kernel.php
+++ app/Port/Middleware/Middlewares/Kernel.php
@@ -56,10 +56,15 @@ class Kernel
             'role'        => \Zizaco\Entrust\Middleware\EntrustRole::class,
             'permission'  => \Zizaco\Entrust\Middleware\EntrustPermission::class,
             'ability'     => \Zizaco\Entrust\Middleware\EntrustAbility::class,
-            'role-web'    => \App\Containers\Authorization\Middlewares\EntrustRoleForWeb::class,
+
+
+            'role.web'    => \App\Containers\Authorization\Middlewares\EntrustRoleForWeb::class,
 
             // Hello API Visitor User Authentication middleware
-            'visitor.auth'  => \App\Containers\ApiAuthentication\Middlewares\VisitorsAuthentication::class,
+            'api.auth.visitor'  => \App\Containers\ApiAuthentication\Middlewares\VisitorsAuthentication::class,
+
+            // Hello API User Authentication middleware for Web Pages
+            'web.auth' => \App\Containers\WebAuthentication\Middlewares\Authenticate::class,
 
             // ...
         ];
