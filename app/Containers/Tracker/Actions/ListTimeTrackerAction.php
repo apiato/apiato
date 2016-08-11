092870bef24160b88c0493d101a82713a5b7ec94
Mahmoud Zalt <mahmoud.z@msn.com>
2016-08-11 16:25:57 +0300

    S: add list tracker action

---
+++ app/Containers/Tracker/Actions/ListTimeTrackerAction.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Containers\Tracker\Actions;
+
+use App\Containers\Tracker\Settings\Repositories\TimeTrackerRepository;
+use App\Containers\User\Services\FindUserService;
+use App\Port\Action\Abstracts\Action;
+
+/**
+ * Class ListTimeTrackerAction.
+ *
+ * @author Mahmoud Zalt <mahmoud@zalt.me>
+ */
+class ListTimeTrackerAction extends Action
+{
+
+
+    /**
+     * @var  \App\Containers\Tracker\Actions\TimeTrackerRepository
+     */
+    private $timeTrackerRepository;
+
+    /**
+     * @var  \App\Containers\User\Services\FindUserService
+     */
+    private $findUserService;
+
+    /**
+     * ListTimeTrackerAction constructor.
+     *
+     * @param \App\Containers\Tracker\Actions\TimeTrackerRepository $timeTrackerRepository
+     * @param \App\Containers\User\Services\FindUserService         $findUserService
+     */
+    public function __construct(TimeTrackerRepository $timeTrackerRepository, FindUserService $findUserService)
+    {
+        $this->timeTrackerRepository = $timeTrackerRepository;
+        $this->findUserService = $findUserService;
+    }
+
+    /**
+     * @param array $relations
+     *
+     * @return  mixed
+     */
+    public function all($relations = ['user'])
+    {
+        $this->timeTrackerRepository->with($relations);
+
+        return $this->timeTrackerRepository->paginate();
+    }
+
+
+}
