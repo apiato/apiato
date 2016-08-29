413b1b6a99def1bcf6a7f42382173d51f907a2fa
Mahmoud Zalt <mahmoud.z@msn.com>
2016-08-29 19:44:33 +0300

    update user json response

--- app/Containers/User/UI/API/Transformers/UserTransformer.php
+++ app/Containers/User/UI/API/Transformers/UserTransformer.php
@@ -21,13 +21,21 @@ class UserTransformer extends Transformer
     public function transform(User $user)
     {
         return [
-            'id'         => (int)$user->id,
-            'name'       => $user->name,
-            'email'      => $user->email,
-            'confirmed'  => $user->confirmed,
-            'token'      => $user->token,
-            'created_at' => $user->created_at,
-            'updated_at' => $user->updated_at,
+            'id'                   => (int)$user->id,
+            'name'                 => $user->name,
+            'email'                => $user->email,
+            'confirmed'            => $user->confirmed,
+            'nickname'             => $user->nickname,
+            'visitor_id'           => $user->visitor_id,
+            'social_auth_provider' => $user->social_provider,
+            'social_id'            => $user->social_id,
+            'social_avatar'        => [
+                'avatar'   => $user->social_avatar,
+                'original' => $user->social_avatar_original,
+            ],
+            'created_at'           => $user->created_at,
+            'updated_at'           => $user->updated_at,
+            'token'                => $user->token,
         ];
     }
 }
