0b0069286d0a5d503d625c6e56029f0928abf777
Mahmoud Zalt <mahmoud.z@msn.com>
2016-09-09 18:06:58 -0400

    change admin login page colour

--- app/Containers/Authentication/UI/WEB/Views/login.blade.php
+++ app/Containers/Authentication/UI/WEB/Views/login.blade.php
@@ -94,11 +94,7 @@
                 color: #EF3B3A;
             }
             body {
-                background: #76b852; /* fallback for old browsers */
-                background: -webkit-linear-gradient(right, #76b852, #8DC26F);
-                background: -moz-linear-gradient(right, #76b852, #8DC26F);
-                background: -o-linear-gradient(right, #76b852, #8DC26F);
-                background: linear-gradient(to left, #76b852, #8DC26F);
+                background: #ffffff;
                 font-family: "Roboto", sans-serif;
                 -webkit-font-smoothing: antialiased;
                 -moz-osx-font-smoothing: grayscale;
