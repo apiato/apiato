12f2b05a0bec5bd4f8312c2c2f6db92e54c527d0
Mahmoud Zalt <mahmoud.z@msn.com>
2016-10-06 00:27:44 -0400

    add powerful helper functions that gets class fullname from file

--- app/Port/Butler/Portals/PortButler.php
+++ app/Port/Butler/Portals/PortButler.php
@@ -98,4 +98,102 @@ class PortButler
     }
 
 
+    /**
+     * build and return an object
+     *
+     * @param $filePathName
+     *
+     * @return  mixed
+     */
+    public function getClassObjectFromFile($filePathName)
+    {
+        $classString = $this->getClassFullNameFromFile($filePathName);
+
+        $object = new $classString;
+
+        return $object;
+    }
+
+
+    /**
+     * return the name and the namespace of a class from its file path
+     * result example: (string) "I\Am\The\Namespace\Of\This\Class"
+     *
+     * @param $filePathName
+     *
+     * @return  string
+     */
+    public function getClassFullNameFromFile($filePathName)
+    {
+        return $this->getClassNamespaceFromFile($filePathName) . '\\' . $this->getClassNameFromFile($filePathName);
+    }
+
+    /**
+     * get the class namespace using token
+     *
+     * @param $filePathName
+     *
+     * @return  null|string
+     */
+    protected function getClassNamespaceFromFile($filePathName)
+    {
+        $src = file_get_contents($filePathName);
+
+        $tokens = token_get_all($src);
+        $count = count($tokens);
+        $i = 0;
+        $namespace = '';
+        $namespace_ok = false;
+        while ($i < $count) {
+            $token = $tokens[$i];
+            if (is_array($token) && $token[0] === T_NAMESPACE) {
+                // Found namespace declaration
+                while (++$i < $count) {
+                    if ($tokens[$i] === ';') {
+                        $namespace_ok = true;
+                        $namespace = trim($namespace);
+                        break;
+                    }
+                    $namespace .= is_array($tokens[$i]) ? $tokens[$i][1] : $tokens[$i];
+                }
+                break;
+            }
+            $i++;
+        }
+        if (!$namespace_ok) {
+            return null;
+        } else {
+            return $namespace;
+        }
+    }
+
+    /**
+     * get the class name form using token
+     *
+     * @param $filePathName
+     *
+     * @return  mixed
+     */
+    protected function getClassNameFromFile($filePathName)
+    {
+        $php_code = file_get_contents($filePathName);
+
+        $classes = array();
+        $tokens = token_get_all($php_code);
+        $count = count($tokens);
+        for ($i = 2; $i < $count; $i++) {
+            if ($tokens[$i - 2][0] == T_CLASS
+                && $tokens[$i - 1][0] == T_WHITESPACE
+                && $tokens[$i][0] == T_STRING
+            ) {
+
+                $class_name = $tokens[$i][1];
+                $classes[] = $class_name;
+            }
+        }
+
+        return $classes[0];
+    }
+
+
 }
