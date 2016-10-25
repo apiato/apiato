b55b9aa19828c6ac7ea7d5b5f1a414be6ad22d59
Mahmoud Zalt <mahmoud.z@msn.com>
2016-10-25 13:28:45 -0400

    upgrade middleware's

--- app/Port/Middleware/PortHttpMiddleware.php
+++ app/Port/Middleware/PortHttpMiddleware.php
@@ -2,7 +2,6 @@
 
 namespace App\Port\Middleware;
 
-use App\Port\Middleware\Middlewares\Kernel;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Http\Kernel as LaravelHttpKernel;
 use Illuminate\Routing\Router;
@@ -10,40 +9,84 @@ use Illuminate\Routing\Router;
 /**
  * Class PortHttpMiddleware
  *
- * A.K.A (app/Http/Port.php)
+ * A.K.A (app/Http/Kernel.php)
  *
  * @author  Mahmoud Zalt  <mahmoud@zalt.me>
  */
 class PortHttpMiddleware extends LaravelHttpKernel
 {
-
     /**
      * The application's global HTTP middleware stack.
      *
      * @var array
      */
-    protected $middleware = [];
+    protected $middleware = [
+        // Laravel default middlewares
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+
+        // Hello API Localization middleware
+        \App\Port\Middleware\Middlewares\Http\Localization::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            // Laravel default WEB middleware's (provided by laravel/laravel)
+            \App\Port\Middleware\Middlewares\Http\EncryptCookies::class,
+            \App\Port\Middleware\Middlewares\Http\VerifyCsrfToken::class,
+
+            // Laravel default WEB middleware's (provided by laravel/framework)
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+        'api' => [
+            // Laravel default API middleware's
+            'throttle:60,1',
+            'bindings',
+
+            // CORS Package middleware
+            \Barryvdh\Cors\HandleCors::class,
+        ],
+    ];
 
     /**
      * The application's route middleware.
      *
      * @var array
      */
-    protected $routeMiddleware = [];
+    protected $routeMiddleware = [
+        // Laravel default route middleware's
+        'can'         => \Illuminate\Auth\Middleware\Authorize::class,
+        'bindings'    => \Illuminate\Routing\Middleware\SubstituteBindings::class,
 
-    /**
-     * PortHttpMiddleware constructor.
-     *
-     * @param \Illuminate\Contracts\Foundation\Application $app
-     * @param \Illuminate\Routing\Router                   $router
-     */
-    public function __construct(Application $app, Router $router, Kernel $kernel)
-    {
-        $this->middleware = $kernel->applicationMiddlewares();
-        $this->routeMiddleware = $kernel->routeMiddlewares();
+        // JWT Package middleware's
+        'jwt.auth'    => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
 
-        parent::__construct($app, $router);
-    }
+        // Entrust Package middleware's
+        'role'        => \Zizaco\Entrust\Middleware\EntrustRole::class,
+        'permission'  => \Zizaco\Entrust\Middleware\EntrustPermission::class,
+        'ability'     => \Zizaco\Entrust\Middleware\EntrustAbility::class,
 
+        // By Hello API
+        'role.web'                => \App\Containers\Authorization\Middlewares\EntrustRoleForWeb::class,
+
+        // Hello API Visitor User Authentication middleware
+        'api.auth.visitor'        => \App\Containers\Authentication\Middlewares\VisitorsAuthentication::class,
+
+        // Hello API User Authentication middleware for Web Pages
+        'web.auth'                => \App\Containers\Authentication\Middlewares\Authentication::class,
+
+        // Hello API Requests Monitor
+        'mobile.requests.monitor' => \App\Containers\Debugger\Middlewares\RequestsMonitorMiddleware::class,
+
+        // ...
+    ];
 
 }
