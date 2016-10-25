39c49f72687be284dbb2dde872874abf5e69db3d
Mahmoud Zalt <mahmoud.z@msn.com>
2016-10-25 12:49:16 -0400

    rename exception handler to port exception handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -38,7 +38,7 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Port\Exception\Handler\ExceptionsHandler::class
+    App\Port\Exception\Handler\PortExceptionsHandler::class
 );
 
 /*
