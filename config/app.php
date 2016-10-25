9e556b8d72c79a56cb99c3dbf35c4df3d4bee4a9
Mahmoud Zalt <mahmoud.z@msn.com>
2016-10-25 11:35:24 -0400

    register NotificationServiceProvider in the providers

--- config/app.php
+++ config/app.php
@@ -152,6 +152,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
 
         /*
          * Third-party packages Service Providers
