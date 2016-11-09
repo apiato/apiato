32611d5817df0e923e3f8a591372f009ec1b1717
Mahmoud Zalt <mahmoud.z@msn.com>
2016-11-09 11:29:11 -0500

    create php file to hold api doc js definitions

---
+++ app/Port/Extra/ApiDocJsDefinitions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @apiDefine SingleUserSuccessResponse
+ * @apiSuccessExample  {json}       Success-Response:
+HTTP/1.1 200 OK
+{
+  "data": {
+    "id": 3,
+    "name": "Mahmoud Zalt",
+    "email": "mahmoud@zalt.me",
+    "confirmed": null,
+    "created_at": {
+      "date": "2016-11-09 16:20:30.000000",
+      "timezone_type": 3,
+      "timezone": "UTC"
+    },
+    "updated_at": {
+      "date": "2016-11-09 16:20:30.000000",
+      "timezone_type": 3,
+      "timezone": "UTC"
+    },
+    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cL2FwaS5wb21zLmRldlwvdXNlclwvcmVnaXN0ZXIiLCJpYXQiOjE0Nzg3MDg0MzAsImV4cCI6MTQ4MTMzNjQzMCwibmJmIjoxNDc4NzA4NDMwLCJqdGkiOiI2NmE2ZGM0N2RkNmRjMDBjZGY5ZGQyZWUwNTdhZGVhMCJ9.j_IZWRWzHQdoWl_YQUxEs0cPz9NEG48baebK1-8TRUY"
+  }
+}
+ */
+
+
