fa5ab53ca801282fcd96efdfaecaa55baba161c4
Mahmoud Zalt <mahmoud.z@msn.com>
2016-11-14 14:28:50 -0500

    update internal external endpoints

--- app/Containers/User/UI/API/Routes/user.v1.internal.php
+++ app/Containers/User/UI/API/Routes/user.v1.internal.php
@@ -77,6 +77,69 @@ $router->get('users', [
     ],
 ]);
 
+/*********************************************************************************
+ * @apiGroup           Users
+ * @apiName            RefreshUser
+ * @api                {post} /users/refresh Refresh User data
+ * @apiDescription     Request the latest user Data. You can send the
+ * `token` header or `user_id` parameter to get the updated user data.
+ * (You can use this Endpoint whenever the user object is updated for any reason to get
+ * his updated data).
+ * @apiVersion         1.0.0
+ * @apiPermission      Authenticated User
+ * @apiHeader          Accept application/json (required)
+ * @apiHeader          Authorization Bearer {User-Token} (required)
+ * @apiParam           user_id User Id (optional)
+ * @apiUse             SingleUserSuccessResponse
+ */
+$router->post('users/refresh', [
+    'uses' => 'Controller@refreshUser',
+    'middleware' => [
+        'api.auth',
+    ],
+]);
+
+/*********************************************************************************
+ * @apiGroup           Users
+ * @apiName            UpdateUser
+ * @api                {put} /users Update User
+ * @apiVersion         1.0.0
+ * @apiPermission      Authenticated User
+ * @apiHeader          Accept application/json (required)
+ * @apiHeader          Authorization Bearer {User-Token} (required)
+ * @apiParam           {String}  password
+ * @apiParam           {String}  name
+ * @apiUse             SingleUserSuccessResponse
+ */
+$router->put('users', [
+    'uses'       => 'Controller@updateUser',
+    'middleware' => [
+        'api.auth',
+    ],
+]);
+
+/*********************************************************************************
+ * @apiGroup           Users
+ * @apiName            registerVisitor
+ * @api                {post} /visitor/register Register visitor (even if he exist)
+ * @apiDescription     This endpoint must be called on App startup. (when the App
+ * allows using it before registering). The endpoint will create a user record
+ * if not already exist based on his unique visitor-id (A.K.A device ID) and return
+ * the `User ID`. Later when the user is required to register, we simply
+ * update his existing record with his information (email, password,...).
+ * @apiVersion         1.0.0
+ * @apiPermission      none
+ * @apiHeader          Accept application/json (required)
+ * @apiHeader          visitor-id The Device ID [12345] (required)
+ * @apiUse             SingleUserSuccessResponse
+ */
+$router->post('visitor/register', [
+    'uses'  => 'Controller@registerVisitor',
+]);
+
+
+
+
 
 
 
