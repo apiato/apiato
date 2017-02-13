9272a30ab300a9c1ad2bd434ce4deb5da7ac590b
Johan Alvarez <llstarscreamll@hotmail.com>
2017-02-13 18:05:14 -0500

    updating to views namespaces

--- app/Containers/Authentication/UI/WEB/Controllers/Controller.php
+++ app/Containers/Authentication/UI/WEB/Controllers/Controller.php
@@ -22,7 +22,7 @@ class Controller extends PortWebController
      */
     public function showLoginPage()
     {
-        return view('login');
+        return view('authentication::login');
     }
 
     /**
@@ -40,7 +40,7 @@ class Controller extends PortWebController
         }
 
         if (is_array($result)) {
-            return view('login')->with($result);
+            return view('authentication::login')->with($result);
         }
 
         return redirect('dashboard');
@@ -53,7 +53,7 @@ class Controller extends PortWebController
      */
     public function viewDashboardPage(ViewDashboardRequest $request)
     {
-        return view('dashboard');
+        return view('authentication::dashboard');
     }
 
     /**
@@ -65,7 +65,7 @@ class Controller extends PortWebController
     {
         $action->run();
 
-        return view('login');
+        return view('authentication::login');
     }
 
 
--- app/Containers/Authentication/UI/WEB/Views/dashboard.blade.php
+++ app/Containers/Authentication/UI/WEB/Views/dashboard.blade.php
@@ -1,45 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('admin::layouts.right-sidebar')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
+@section('content')
+<div class="content animated fadeIn">
+    <div class="jumbotron">
         <div class="container">
-            <div class="content">
-                <div class="title">Hello Admin</div>
-            </div>
+        	<h1 class="text-center">Hello Admin</h1>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
