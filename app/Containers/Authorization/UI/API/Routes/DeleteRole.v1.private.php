6cd5b8ef31fea6fd1f09096fd66cf89ac17b7fd2
Mahmoud Zalt <mahmoud.z@msn.com>
2017-02-15 13:37:55 -0500

    fix docblock for delete role

--- app/Containers/Authorization/UI/API/Routes/DeleteRole.v1.private.php
+++ app/Containers/Authorization/UI/API/Routes/DeleteRole.v1.private.php
@@ -4,7 +4,7 @@
  * @apiGroup           RolePermission
  * @apiName            deleteRole
  * @api                {delete} /roles/:name Delete Role
- * @apiDescription     Delete Role by ID
+ * @apiDescription     Delete Role by name
  * @apiVersion         1.0.0
  * @apiPermission      Authenticated Role
  *
@@ -14,7 +14,7 @@
  * @apiSuccessExample  {json}       Success-Response:
  * HTTP/1.1 202 OK
 {
-    "message": "Role (14) Deleted Successfully."
+    "message": "Role (manager) Deleted Successfully."
 }
  */
 
