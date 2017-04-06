23e889eba4f15498819dc904964a467bf75d7966
Mahmoud Zalt <mahmoud.z@msn.com>
2017-04-06 15:50:11 -0400

    return null when user token not avaialble

--- app/Containers/User/UI/API/Transformers/UserTransformer.php
+++ app/Containers/User/UI/API/Transformers/UserTransformer.php
@@ -48,7 +48,7 @@ class UserTransformer extends Transformer
             ],
             'created_at'           => $user->created_at,
             'updated_at'           => $user->updated_at,
-            'token'                => App::make(TokenTransformer::class)->transform($user->access_token),
+            'token'                => $user->access_token ? App::make(TokenTransformer::class)->transform($user->access_token) : null,
         ];
 
         $response = $this->ifAdmin([
