7915fbb52840d5deb436c7752e894084d4a121b9
Sven Slijkoord <sven.sliijkoord@gmail.com>
2017-05-12 18:09:53 +0000

    Refactored ValidationFailedException class to extend from the parent exception in the ship.

--- app/Ship/Exceptions/ValidationFailedException.php
+++ app/Ship/Exceptions/ValidationFailedException.php
@@ -2,37 +2,17 @@
 
 namespace App\Ship\Exceptions;
 
-use Dingo\Api\Exception\ResourceException as DingoResourceException;
-use Exception;
-use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use App\Ship\Parents\Exceptions\Exception;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class ValidationFailedException.
  *
- * Note: exceptionally extending from `Dingo\Api\Exception\ResourceException` instead of
- * `App\Ship\Parents\Exceptions\Exception`. To keep the request validation
- * throwing well formatted error. To be debugged later and switched to extending from
- * `Exception` while carefully looking at the validation response error format.
- *
- * @author  Mahmoud Zalt <mahmoud@zalt.me>
+ * @author Mahmoud Zalt <mahmoud@zalt.me>
  */
-class ValidationFailedException extends DingoResourceException
+class ValidationFailedException extends Exception
 {
-
-    public $httpStatusCode = SymfonyResponse::HTTP_UNPROCESSABLE_ENTITY;
+    public $httpStatusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
 
     public $message = 'Invalid Input.';
-
-    /**
-     * ValidationFailedException constructor.
-     *
-     * @param null            $errors
-     * @param \Exception|null $previous
-     * @param array           $headers
-     * @param int             $code
-     */
-    public function __construct($errors = null, Exception $previous = null, $headers = [], $code = 0)
-    {
-        parent::__construct($this->message, $errors, $previous, $headers, $code);
-    }
 }
