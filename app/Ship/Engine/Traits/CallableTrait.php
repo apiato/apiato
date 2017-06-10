5fee79c89d5241c32b000af0ae1b2024fee24edc
Mahmoud Zalt <mahmoud@zalt.me>
2017-06-10 15:31:11 +0300

    support calling other class methods before the run using the `call`

--- app/Ship/Engine/Traits/CallableTrait.php
+++ app/Ship/Engine/Traits/CallableTrait.php
@@ -3,6 +3,8 @@
 namespace App\Ship\Engine\Traits;
 
 use Illuminate\Support\Facades\App;
+use function is_array;
+use function key;
 
 /**
  * Class CallableTrait.
@@ -14,22 +16,40 @@ trait CallableTrait
 
     /**
      * @param       $class
-     * @param array $args
+     * @param array $runArguments
+     * @param array $methods
      *
      * @return  mixed
      */
-    public function call($class, $args = [])
+    public function call($class, $runArguments = [], $methods = [])
     {
         $action = App::make($class);
 
-        if(method_exists($action, 'setUI')){
-            // $this->ui is coming, should be attached on the parent controller, from where the actions was called.
-            // It can be WebController and ApiController. Each of them has ui, to inform the action
-            // if it needs to handle the request differently.
+        // $this->ui is coming, should be attached on the parent controller, from where the actions was called.
+        // It can be WebController and ApiController. Each of them has ui, to inform the action
+        // if it needs to handle the request differently.
+        if (method_exists($action, 'setUI')) {
             $action->setUI($this->ui);
         }
 
-        return $action->run(...$args);
+        // allows calling other methods in the class before calling the main `run` function.
+        foreach ($methods as $methodInfo) {
+            // if is array means it has arguments
+            if (is_array($methodInfo)) {
+                $method = key($methodInfo);
+                $arguments = $methodInfo[$method];
+                if (method_exists($action, $method)) {
+                    $action->$method(...$arguments);
+                }
+            } else {
+                // if is string means it's just the function name
+                if (method_exists($action, $methodInfo)) {
+                    $action->$methodInfo();
+                }
+            }
+        }
+
+        return $action->run(...$runArguments);
     }
 
 }
