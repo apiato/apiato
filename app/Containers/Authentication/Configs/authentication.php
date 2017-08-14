29133d32b06add629a9829780f7f55889dcd7bf2
Mahmoud Zalt <mahmoudz.me@gmail.com>
2017-08-14 16:19:43 +0300

    set login_only_confirmed_users to false by default

--- app/Containers/Authentication/Configs/authentication.php
+++ app/Containers/Authentication/Configs/authentication.php
@@ -2,6 +2,16 @@
 
 return [
 
-    'login_only_confirmed_users' => true,
+    /*
+    |--------------------------------------------------------------------------
+    | Email Confirmation
+    |--------------------------------------------------------------------------
+    | 
+    | When set to true, the user must confirm his email before being able to 
+    | Login the first time, after registrtion.
+    | 
+    */    
+  
+    'login_only_confirmed_users' => false,
 
 ];
