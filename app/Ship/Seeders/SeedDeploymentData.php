a09836955ab952c6559fa4379e539e63d3fa7080
Johannes Schobel <johannes.schobel@uni-ulm.de>
2017-08-15 17:44:02 +0200

    new seeder

---
+++ app/Ship/Seeders/SeedDeploymentData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Ship\Seeders;
+
+use App\Ship\Parents\Seeders\Seeder;
+
+/**
+ * Class SeedDeploymentData
+ *
+ * @author  Johannes Schobel <johannes.schobel@googlemail.com>
+ */
+class SeedDeploymentData extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create data for live deployment here
+    }
+
+}
