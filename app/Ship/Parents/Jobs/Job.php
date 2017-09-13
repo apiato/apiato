ed575060d7d5b857b771fe977ec5d8bd6b071c72
Mahmoud Zalt <mahmoud@zalt.me>
2017-09-13 03:59:50 +0300

    update Job parent class move Queueable from Core Abstract to in class

--- app/Ship/Parents/Jobs/Job.php
+++ app/Ship/Parents/Jobs/Job.php
@@ -3,13 +3,32 @@
 namespace App\Ship\Parents\Jobs;
 
 use Apiato\Core\Abstracts\Jobs\Job as AbstractJob;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
 
 /**
  * Class Job
  *
+ * A.K.A (app/Jobs/Job.php)
+ *
  * @author  Mahmoud Zalt  <mahmoud@zalt.me>
  */
-abstract class Job extends AbstractJob
+abstract class Job extends AbstractJob implements ShouldQueue
 {
-    
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /*
+    |--------------------------------------------------------------------------
+    | Queueable Jobs
+    |--------------------------------------------------------------------------
+    |
+    | This job base class provides a central location to place any logic that
+    | is shared across all of your jobs. The trait included with the class
+    | provides access to the "onQueue" and "delay" queue helper methods.
+    |
+    */
+
 }
