6290c28c2a2a4dc853c6692dede14551d4cd98b5
Johannes Schobel <johannes.schobel@uni-ulm.de>
2017-09-16 15:18:55 +0200

    remove ship includes from usermodel

--- app/Ship/Parents/Models/UserModel.php
+++ app/Ship/Parents/Models/UserModel.php
@@ -6,8 +6,6 @@ use Apiato\Core\Abstracts\Models\UserModel as AbstractUserModel;
 use Apiato\Core\Traits\HashIdTrait;
 use Apiato\Core\Traits\HasResourceKeyTrait;
 use App\Containers\Authorization\Traits\AuthorizationTrait;
-use App\Ship\Payment\Contracts\ChargeableInterface;
-use App\Ship\Payment\Traits\ChargeableTrait;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
