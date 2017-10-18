58b308ed4dc30b5c5541ab58cef83f2f5d686852
Johannes Schobel <johannes.schobel@uni-ulm.de>
2017-10-18 08:46:14 +0200

    change middleware slightly

--- app/Ship/Middlewares/Http/ValidateJsonContent.php
+++ app/Ship/Middlewares/Http/ValidateJsonContent.php
@@ -26,27 +26,30 @@ class ValidateJsonContent extends Middleware
      */
     public function handle(Request $request, Closure $next)
     {
-        // get the response after the request is done
-        $response = $next($request);
-
+        $acceptHeader = $request->header('accept');
         $contentType = 'application/json';
+
+        // check if the accept header is set to application/json
+        if (strpos($acceptHeader, $contentType) === false) {
+            // if forcing users to have the accept header is enabled, then throw an exception
+            if (Config::get('apiato.requests.force-accept-header')) {
+                throw new MissingJSONHeaderException();
+            }
+        }
+
+        // the request has to be processed, so get the response after the request is done
+        $response = $next($request);
 
         // set Content Languages header in the response | always return Content-Type application/json in the header
         $response->headers->set('Content-Type', $contentType);
 
-        $acceptHeader = $request->header('accept');
-
         // if request doesn't contain in header accept = application/json. Return a warning in the response
         if (strpos($acceptHeader, $contentType) === false) {
-
-            // if forcing users to have the accept header is enabled, then throw an exception else just send warning
-            if(Config::get('apiato.requests.force-accept-header')){
-                throw new MissingJSONHeaderException();
-            }
 
             $warnCode = '199'; // https://www.iana.org/assignments/http-warn-codes/http-warn-codes.xhtml
             $warnMessage = 'Missing request header [ accept = ' . $contentType . ' ] when calling a JSON API.';
             $response->headers->set('Warning', $warnCode . ' ' . $warnMessage);
+
         }
 
         // return the response
