afa1fdb49849c47e5d5ba9ffff744d598d2a8c7c
Johannes Schobel <johannes.schobel@uni-ulm.de>
2017-11-22 09:33:21 +0100

    get rid of env() calls

--- app/Containers/Authentication/Configs/authentication-container.php
+++ app/Containers/Authentication/Configs/authentication-container.php
@@ -14,4 +14,30 @@ return [
   
     'require_email_confirmation' => false,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Clients
+    |--------------------------------------------------------------------------
+    |
+    | A list of clients that have access to the application.
+    |
+    */
+
+    'clients' => [
+        'web' => [
+            'admin' => [
+                'id' => env('CLIENT_WEB_ADMIN_ID'),
+                'secret' => env('CLIENT_WEB_ADMIN_SECRET'),
+            ],
+        ],
+        'mobile' => [
+            'admin' => [
+                'id' => env('CLIENT_MOBILE_ADMIN_ID'),
+                'secret' => env('CLIENT_MOBILE_ADMIN_SECRET'),
+            ],
+        ],
+
+        // add your other clients here
+    ],
+
 ];
