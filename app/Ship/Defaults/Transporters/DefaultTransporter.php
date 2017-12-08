745f63da1417c64b0c94536c1745a80443998812
Johannes Schobel <johannes.schobel@uni-ulm.de>
2017-12-08 17:16:32 +0100

    add Transporters to SHIP

---
+++ app/Ship/Defaults/Transporters/DefaultTransporter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Ship\Defaults\Transporters;
+
+use App\Ship\Parents\Transporters\Transporter;
+
+/**
+ * Class DefaultTransporter
+ *
+ * @author Johannes Schobel <johannes.schobel@googlemail.com>
+ */
+class DefaultTransporter extends Transporter
+{
+
+}
