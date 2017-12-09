2bb57f5d44bc2e66d5f83f539c9266ed32408557
Johannes Schobel <johannes.schobel@uni-ulm.de>
2017-12-09 15:20:08 +0100

    change default transporter and add it to the request

--- app/Ship/Defaults/Transporters/DefaultTransporter.php
+++ app/Ship/Defaults/Transporters/DefaultTransporter.php
@@ -11,5 +11,20 @@ use App\Ship\Parents\Transporters\Transporter;
  */
 class DefaultTransporter extends Transporter
 {
+    /*
+     * README FIRST and be sure to fully understand this concept!
+     * This is the default transporter that is used, if no other one is specified in the REQUEST class itself. This Transporter,
+     * in turn, makes no restrictions to the properties; i.e., it allows everything to be written. This may be ok for
+     * some use-cases, however, you may also want to create your own, more specifically tailored Transporters.
+     */
 
+    /**
+     * @var array
+     */
+    protected $schema = [
+        'type' => 'object',
+        'properties' => [
+            'additionalProperties' => true,
+        ],
+    ];
 }
