858afa276905468a0606aa800d81f3f929ad60e3
Mahmoud Zalt <mahmoud@zalt.me>
2017-12-09 04:02:26 +0100

    add sample command on the Ship as a demo as well

---
+++ app/Ship/Commands/HelloWorldCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Ship\Commands;
+
+use App\Ship\Parents\Commands\ConsoleCommand;
+
+/**
+ * Class HelloWorldCommand
+ *
+ * @author  Mahmoud Zalt  <mahmoud@zalt.me>
+ */
+class HelloWorldCommand extends ConsoleCommand
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hello:world';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Hello World!';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        echo "Hello World :)\n";
+    }
+}
