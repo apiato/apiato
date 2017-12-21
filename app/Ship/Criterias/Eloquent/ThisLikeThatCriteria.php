c07dd26a2384b42d51978e1551ae8a644bc81b47
Fabian Widmann <fabian.widmann@uni-ulm.de>
2017-12-21 15:38:58 +0100

    Fixed the "ThisLikeThat" Criteria to now correctly be enclosed in parantheses.
    Example:
    Find all data whose type is like temperature or wind, where the source is equal to X:
    
    select * from `weather_data` where (`type` LIKE %temperature% or `type` LIKE %wind%) and `source` = X"
    
    Without the parantheses this would have been evaluted wrongly and would lead to also display temperature objects that do not have the source listed as X.

--- app/Ship/Criterias/Eloquent/ThisLikeThatCriteria.php
+++ app/Ship/Criterias/Eloquent/ThisLikeThatCriteria.php
@@ -3,12 +3,10 @@
 namespace App\Ship\Criterias\Eloquent;
 
 use App\Ship\Parents\Criterias\Criteria;
-use Illuminate\Database\Query\Builder;
-use Prettus\Repository\Contracts\RepositoryInterface as PrettusRepositoryInterface;
 
 /**
  * Class ThisLikeThatCriteria
- * 
+ *
  * @author Fabian Widmann <fabian.widmann@gmail.com>
  *
  * Retrieves all entities where $field contains one or more of the given items in $valueString.
@@ -41,25 +39,26 @@ class ThisLikeThatCriteria extends Criteria
     {
         $this->field = $field;
         $this->valueString = $valueString;
-        $this->separator =$separator;
-        $this->wildcard =$wildcard;
+        $this->separator = $separator;
+        $this->wildcard = $wildcard;
     }
 
     /**
      * Applies the criteria - if more than one value is separated by the configured separator we will "OR" all the params.
-     * 
+     *
      * @param  $model
      * @param  $repository
-     * 
+     *
      * @return  mixed
      */
     public function apply($model, $repository)
     {
-        $values = explode(config($this->separator), $this->valueString);
-        $model = $model->where($this->field, 'LIKE', str_replace($this->wildcard, '%', array_shift($values)));
-        foreach ($values as $value)
-            $model = $model->orWhere($this->field, 'LIKE', str_replace($this->wildcard, '%', $value));
-        return $model;
+        return $model->where(function ($query) {
+            $values = explode($this->separator, $this->valueString);
+            $query->where($this->field, 'LIKE', str_replace($this->wildcard, '%', array_shift($values)));
+            foreach ($values as $value)
+                $query->orWhere($this->field, 'LIKE', str_replace($this->wildcard, '%', $value));
+        });
     }
 
 }
