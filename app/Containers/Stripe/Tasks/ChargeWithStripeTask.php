7774564adc3580b115b380ec7ad555c34ae21de7
Johannes Schobel <johannes.schobel@uni-ulm.de>
2018-01-04 12:38:10 +0100

    adapt existing Charger Tasks (Stripe, WePay) to follow the PaymentTransaction approach

--- app/Containers/Stripe/Tasks/ChargeWithStripeTask.php
+++ app/Containers/Stripe/Tasks/ChargeWithStripeTask.php
@@ -5,6 +5,7 @@ namespace App\Containers\Stripe\Tasks;
 use App\Containers\Payment\Contracts\ChargeableInterface;
 use App\Containers\Payment\Contracts\PaymentChargerInterface;
 use App\Containers\Payment\Models\AbstractPaymentAccount;
+use App\Containers\Payment\Models\PaymentTransaction;
 use App\Containers\Stripe\Exceptions\StripeAccountNotFoundException;
 use App\Containers\Stripe\Exceptions\StripeApiErrorException;
 use App\Ship\Parents\Tasks\Task;
@@ -38,18 +39,18 @@ class ChargeWithStripeTask extends Task implements PaymentChargerInterface
      * @param float                                                 $amount
      * @param string                                                $currency
      *
-     * @return array|null
+     * @return PaymentTransaction
      * @throws StripeAccountNotFoundException
      * @throws StripeApiErrorException
      */
-    public function charge(ChargeableInterface $user, AbstractPaymentAccount $account, $amount, $currency = 'USD')
+    public function charge(ChargeableInterface $user, AbstractPaymentAccount $account, $amount, $currency = 'USD') : PaymentTransaction
     {
         // NOTE: you should not call this function directly. Instead use the Payment Gateway in the Payment container.
         // Or even better to use the charge function in the ChargeableTrait.
 
         $valid = $account->checkIfPaymentDataIsSet(['customer_id', 'card_id', 'card_funding', 'card_last_digits', 'card_fingerprint']);
 
-        if($valid == false) {
+        if ($valid == false) {
             throw new StripeAccountNotFoundException('We could not find your credit card information. 
             For security reasons, we do not store your credit card information on our server. 
             So please login to our Web App and enter your credit card information directly into Stripe, 
@@ -73,14 +74,18 @@ class ChargeWithStripeTask extends Task implements PaymentChargerInterface
         }
 
         if ($response['paid'] !== true) {
-            return null;
+            throw new StripeApiErrorException('Payment was not set to PAID.');
         }
 
         // this data will be stored on the pivot table (user credits)
-        return [
-            'payment_method' => 'stripe',
-            'description'    => $response['id'] // the charge id
-        ];
+        $transaction = new PaymentTransaction([
+            'transaction_id' => $response['id'],
+            'status' => $response['status'],
+            'is_successful' => true,
+            'data' => $response,
+        ]);
+
+        return $transaction;
     }
 
 }
