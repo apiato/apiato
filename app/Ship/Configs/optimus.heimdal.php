aa467437d309dca14a443eda6de1b6f8b59ce060
Johannes Schobel <johannes.schobel@uni-ulm.de>
2018-04-09 11:39:48 +0200

    add formatter for new exception

--- app/Ship/Configs/optimus.heimdal.php
+++ app/Ship/Configs/optimus.heimdal.php
@@ -9,10 +9,10 @@ return [
     'formatters' => [
         // insert your custom Exception Formatters here!
 
-
         // Apiato Exception Formatters below
         \Illuminate\Validation\ValidationException::class => \App\Ship\Exceptions\Formatters\ValidationExceptionFormatter::class,
         SymfonyException\UnprocessableEntityHttpException::class => \App\Ship\Exceptions\Formatters\UnprocessableEntityHttpExceptionFormatter::class,
+        \App\Containers\Authentication\Exceptions\AuthenticationException::class => \App\Ship\Exceptions\Formatters\AuthenticationExceptionFormatter::class,
         Illuminate\Auth\AuthenticationException::class => \App\Ship\Exceptions\Formatters\AuthenticationExceptionFormatter::class,
         Illuminate\Auth\Access\AuthorizationException::class => \App\Ship\Exceptions\Formatters\AuthorizationExceptionFormatter::class,
         SymfonyException\MethodNotAllowedHttpException::class => \App\Ship\Exceptions\Formatters\MethodNotAllowedExceptionFormatter::class,
