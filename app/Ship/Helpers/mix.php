7e9c3cf6f92f889699a011c50672ca4fac4fcd36
dmekhov <dmekhov@gmail.com>
2018-06-30 10:44:57 +0300

    Style fixes

--- app/Ship/Helpers/mix.php
+++ app/Ship/Helpers/mix.php
@@ -2,18 +2,18 @@
 
 // Add your helper functions here...
 
-if (! function_exists('route')) {
+if (!function_exists('route')) {
     /**
      * Generate the URL to a named route with lang parameter.
      *
-     * @param  array|string  $name
-     * @param  mixed  $parameters
-     * @param  bool  $absolute
+     * @param  array|string $name
+     * @param  mixed $parameters
+     * @param  bool $absolute
      * @return string
      */
     function route($name, $parameters = [], $absolute = true)
     {
-        if(config('apiato.web.use_localized_routes') && !array_has($parameters, 'lang')) {
+        if (config('apiato.web.use_localized_routes') && !array_has($parameters, 'lang')) {
             $parameters = array_add($parameters, 'lang', app()->getLocale());
         }
 
