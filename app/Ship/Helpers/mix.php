35ff44b6681b511ba985d707a940f45f5c09e948
Johnathan Douglas de Souza Santos <johdougss@gmail.com>
2018-07-16 23:33:34 -0300

    Helper apiato
    ```
    // instance \Apiato\Core\Foundation\Apiato
    apiato();
    
    // call
    apiato('Authorization@GetAllPermissionsAction');
    ```

--- app/Ship/Helpers/mix.php
+++ app/Ship/Helpers/mix.php
@@ -1,3 +1,21 @@
 <?php
 
 // Add your helper functions here...
+
+if (!function_exists('apiato')) {
+  /**
+   * @param null $abstract
+   * @param array $parameters
+   * @return \Apiato\Core\Foundation\Apiato|mixed
+   */
+  function apiato($abstract = null, array $parameters = [])
+  {
+    /** @var \Apiato\Core\Foundation\Apiato $apito */
+    $apito = app('Apiato');
+    if (is_null($abstract)) {
+      return $apito;
+    }
+
+    return $apito->call($abstract, $parameters);
+  }
+}
