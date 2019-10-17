66c9724b9b3ce6dcb07d76c56b6e24bc179a95b3
Dominykas Tijūnaitis <dominykas@tijunaitis.lt>
2019-10-17 23:40:40 +0300

    Create auth.php

---
+++ resources/lang/lt/auth.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Prisijungimo vertimai
+    |--------------------------------------------------------------------------
+    |
+    | Šios eilutės naudojamos vertimams, kurie
+    | rodomi atliekant prisijungimo veiksmą.
+    |
+    */
+    'failed'   => 'Prisijungimo duomenys neatitinka.',
+    'throttle' => 'Per daug bandymų prisijungti. Bandykite po :seconds sec.',
+];
