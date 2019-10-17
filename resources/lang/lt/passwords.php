906215cb6ba9a61fb64a3e7e0e6f6ea7c0c3fae6
Dominykas Tijūnaitis <dominykas@tijunaitis.lt>
2019-10-17 23:41:31 +0300

    Create passwords.php

---
+++ resources/lang/lt/passwords.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Slaptažodžio priminimo kalbos eilutės
+    |--------------------------------------------------------------------------
+    |
+    | Sekančios kalbos eilutės yra numatytos elutės, atitinkančios priežastims,
+    | pateikiamoms slatažodžių tarpininko, kai nepavyksta slaptažodžio atnaujinimo
+    | bandymas, tokioms kaip negaliojanti žymė ar neteisingas naujas slaptažodis..
+    |
+    */
+    'password' => 'Slaptažodis turi būti bent aštuonių simbolių ir sutapti su patvirtinimu.',
+    'reset'    => 'Nustatytas naujas slaptažodis!',
+    'sent'     => 'Naujo slaptažodžio nustatymo nuoroda išsiųsta',
+    'token'    => 'Šis slaptažodžio raktas yra neteisingas.',
+    'user'     => 'Vartotojas su tokiu el. paštu nerastas.',
+];
