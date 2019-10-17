119d681d851c67bf9e2c75e36b767bc7238832bb
Dominykas Tijūnaitis <dominykas@tijunaitis.lt>
2019-10-17 23:41:08 +0300

    Create pagination.php

---
+++ resources/lang/lt/pagination.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Puslapiavimo kalbos eilutės
+    |--------------------------------------------------------------------------
+    |
+    | Šios kalbos eilutės yra naudojamas puslapiavimo bibliotekos kurti
+    | paprastas puslapiavimo nuorodas. Jūs galite laisvai keisti jas
+    | į bet kokias kitas labiau tinkančias Jūsų programai.
+    |
+    */
+    'previous' => '&laquo; Ankstesnis',
+    'next'     => 'Kitas &raquo;',
+];
