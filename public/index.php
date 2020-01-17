663ef4990f6f3120f51e664e4d20b419f740148e
Mahmoudz <mahmoud.al.zalat@cg.nl>
2020-01-17 15:31:37 +0100

    upgrade index.php

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+define('LARAVEL_START', microtime(true));
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -15,7 +17,7 @@
 | Composer provides a convenient, automatically generated class loader for
 | our application. We just need to utilize it! We'll simply require it
 | into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels nice to relax.
+| loading any of our classes later on. It feels great to relax.
 |
 */
 
@@ -50,7 +52,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+  $request = Illuminate\Http\Request::capture()
 );
 
 $response->send();
