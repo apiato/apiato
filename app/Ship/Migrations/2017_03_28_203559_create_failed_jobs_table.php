5bb12bac4b76299480724b9471875d8639c2d21f
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-02-26 18:44:16 +0330

    Update failedjobs migration

--- app/Ship/Migrations/2017_03_28_203559_create_failed_jobs_table.php
+++ app/Ship/Migrations/2017_03_28_203559_create_failed_jobs_table.php
@@ -21,6 +21,7 @@ class CreateFailedJobsTable extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('uuid')->nullable()->unique();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
