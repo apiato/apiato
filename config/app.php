d0a1ff958eaf9b7453fee707d415abf5f416a2d9
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-02-28 22:48:13 +0330

    Add missing aliase

--- config/app.php
+++ config/app.php
@@ -202,6 +202,7 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Http' => Illuminate\Support\Facades\Http::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
@@ -220,7 +221,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
         // NOTE: all packages aliases must be registered inside their containers service providers
     ],
-
 ];
