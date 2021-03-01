da2ca4642ff222d8eb3ed118eb4eb20f0d933498
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-01 22:35:09 +0330

    Add Ship empty Helpers file

---
+++ app/Ship/Helpers/Helpers.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Ship Helpers
+|--------------------------------------------------------------------------
+|
+| Write only general helper functions here.
+| Container specific helper functions should go into their own related Containers.
+| All files under Containers/{container_name}/Helpers/ folder will be autoloaded by Apiato.
+|
+*/
