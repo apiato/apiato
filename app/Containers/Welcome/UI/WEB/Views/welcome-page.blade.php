8c6ce24d17e2403e0bfdc4fc8de27be8b774a855
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-25 13:46:18 +0430

    Remove unused code

--- app/Containers/Welcome/UI/WEB/Views/welcome-page.blade.php
+++ app/Containers/Welcome/UI/WEB/Views/welcome-page.blade.php
@@ -67,17 +67,6 @@
 </head>
 <body>
 <div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
-                <a href="{{ route('register') }}">Register</a>
-            @endauth
-        </div>
-    @endif
-
     <div class="content">
 
         <div class="title m-b-md">Apiato</div>
