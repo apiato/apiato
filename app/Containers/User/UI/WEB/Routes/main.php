5dad35b59eee61be86e662de845d1d0e3eac64c7
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-25 13:46:45 +0430

    Remove Web UI from user container

--- app/Containers/User/UI/WEB/Routes/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Containers\User\UI\API\Controllers\Controller;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user', [Controller::class, 'sayWelcome'])
-    ->name('get_user_home_page');
