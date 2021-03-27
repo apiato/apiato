b1ff43fd14d22e7cfe90f9a0b28580c10e91cb8e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-27 17:34:34 +0430

    Refactor

--- app/Containers/User/Actions/FindUserByIdAction.php
+++ app/Containers/User/Actions/FindUserByIdAction.php
@@ -5,19 +5,12 @@ namespace App\Containers\User\Actions;
 use Apiato\Core\Foundation\Facades\Apiato;
 use App\Containers\User\Models\User;
 use App\Containers\User\UI\API\Requests\FindUserByIdRequest;
-use App\Ship\Exceptions\NotFoundException;
 use App\Ship\Parents\Actions\Action;
 
 class FindUserByIdAction extends Action
 {
     public function run(FindUserByIdRequest $data): User
     {
-        $user = Apiato::call('User@FindUserByIdTask', [$data->id]);
-
-        if (!$user) {
-            throw new NotFoundException();
-        }
-
-        return $user;
+        return Apiato::call('User@FindUserByIdTask', [$data->id]);
     }
 }
