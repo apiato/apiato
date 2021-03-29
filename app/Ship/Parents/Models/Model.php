d42a9fa2b984733f099187a0a9a03fc8c87af06d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-29 16:49:58 +0430

    Move some traits to core

--- app/Ship/Parents/Models/Model.php
+++ app/Ship/Parents/Models/Model.php
@@ -3,21 +3,8 @@
 namespace App\Ship\Parents\Models;
 
 use Apiato\Core\Abstracts\Models\Model as AbstractModel;
-use Apiato\Core\Traits\FactoryLocatorTrait;
-use Apiato\Core\Traits\HashIdTrait;
-use Apiato\Core\Traits\HasResourceKeyTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-/**
- * Class Model.
- *
- * @author  Mahmoud Zalt <mahmoud@zalt.me>
- */
 abstract class Model extends AbstractModel
 {
-    use HashIdTrait;
-    use HasResourceKeyTrait;
-    use HasFactory, FactoryLocatorTrait {
-        FactoryLocatorTrait::newFactory insteadof HasFactory;
-    }
+
 }
--- app/Ship/Parents/Models/UserModel.php
+++ app/Ship/Parents/Models/UserModel.php
@@ -3,29 +3,15 @@
 namespace App\Ship\Parents\Models;
 
 use Apiato\Core\Abstracts\Models\UserModel as AbstractUserModel;
-use Apiato\Core\Traits\FactoryLocatorTrait;
-use Apiato\Core\Traits\HashIdTrait;
-use Apiato\Core\Traits\HasResourceKeyTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-/**
- * Class UserModel.
- *
- * @author  Mahmoud Zalt <mahmoud@zalt.me>
- */
 abstract class UserModel extends AbstractUserModel
 {
     use Notifiable;
     use SoftDeletes;
-    use HashIdTrait;
     use HasRoles;
     use HasApiTokens;
-    use HasResourceKeyTrait;
-    use HasFactory, FactoryLocatorTrait {
-        FactoryLocatorTrait::newFactory insteadof HasFactory;
-    }
 }
