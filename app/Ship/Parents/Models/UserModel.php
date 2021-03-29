de2c4afc1b4a30faaf2a59fc08d3164144f08328
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-29 18:17:17 +0430

    move trait use to parent

--- app/Ship/Parents/Models/UserModel.php
+++ app/Ship/Parents/Models/UserModel.php
@@ -6,12 +6,10 @@ use Apiato\Core\Abstracts\Models\UserModel as AbstractUserModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 
 abstract class UserModel extends AbstractUserModel
 {
     use Notifiable;
     use SoftDeletes;
-    use HasRoles;
     use HasApiTokens;
 }
