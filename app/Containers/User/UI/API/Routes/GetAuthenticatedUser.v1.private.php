da3c3f109e7f13d21c5cf5befd671552f01abec2
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-04-07 13:23:04 +0430

    doc update

--- app/Containers/User/UI/API/Routes/GetAuthenticatedUser.v1.private.php
+++ app/Containers/User/UI/API/Routes/GetAuthenticatedUser.v1.private.php
@@ -8,7 +8,7 @@
  * @apiDescription     Find the user details of the logged in user from its Token. (without specifying his ID)
  *
  * @apiVersion         1.0.0
- * @apiPermission      none
+ * @apiPermission      Authenticated User
  *
  * @apiUse             UserSuccessSingleResponse
  */
