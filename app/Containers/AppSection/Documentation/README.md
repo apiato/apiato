cd246da822fd29dea570110de43cfa35a197f298
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-04-10 15:03:27 +0430

    Extract documentation container to its own repo

--- app/Containers/AppSection/Documentation/README.md
+++
@@ -1,1 +0,0 @@
-### Apiato Documentation Container
--- app/Containers/AppSection/Documentation/UI/API/Routes/__generalAccepted.v1.public.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @apiDefine GeneralAcceptedResponse
- * @apiSuccessExample {json} Success-Response:
-HTTP/1.1 202 Accepted
-{
-}
- */
