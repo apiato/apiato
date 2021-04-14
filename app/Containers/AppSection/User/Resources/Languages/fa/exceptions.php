d98251a132b9bbfa425ce5396e45833c08cefa18
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-04-14 11:46:40 +0430

    Removed localizations

--- app/Containers/AppSection/User/Resources/Languages/fa/exceptions.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'userNotFoundException' => 'کاربر یافت نشد',
-];
--- app/Containers/AppSection/User/Tasks/FindUserByIdTask.php
+++ app/Containers/AppSection/User/Tasks/FindUserByIdTask.php
@@ -22,7 +22,7 @@ class FindUserByIdTask extends Task
         try {
             $user = $this->repository->find($userId);
         } catch (Exception $e) {
-            throw (new NotFoundException())->withErrors(['404' => 'user::exceptions.userNotFoundException']);
+            throw (new NotFoundException())->withErrors(['404' => 'User not found']);
         }
 
         return $user;
