a97eca71554d4448f7e0f198ca981cc13c5c061a
Ali Kazmi <ali@kazmi.me>
2021-05-18 14:25:37 +0500

    Update user repository to add model function to return user model specified in configuration
    This commit adds a model function in the user repository class to return the user model specified in configurations instead of picking it from the AppSection/User container.

--- app/Containers/AppSection/User/Data/Repositories/UserRepository.php
+++ app/Containers/AppSection/User/Data/Repositories/UserRepository.php
@@ -13,4 +13,9 @@ class UserRepository extends Repository
         'email_verified_at' => '=',
         'created_at' => 'like',
     ];
+    
+    public function model(): string
+    {
+        return config('auth.providers.users.model');
+    }
 }
