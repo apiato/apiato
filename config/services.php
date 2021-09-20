ffe047974d70c30b1a9aa343a99cc126c931cff5
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-20 13:04:36 +0430

    remove unused config (related to social auth container)

--- config/services.php
+++ config/services.php
@@ -29,10 +29,4 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
-    'google' => [
-        'client_id' => env('AUTH_GOOGLE_CLIENT_ID'),
-        'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('AUTH_GOOGLE_CLIENT_REDIRECT'),
-    ],
 ];
