6b247489246acc6e43b8445477d66e45457432d8
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-23 13:49:40 +0330

    add docblock

--- app/Containers/AppSection/User/Actions/GetAllUsersAction.php
+++ app/Containers/AppSection/User/Actions/GetAllUsersAction.php
@@ -2,11 +2,17 @@
 
 namespace App\Containers\AppSection\User\Actions;
 
+use Apiato\Core\Exceptions\CoreInternalErrorException;
 use App\Containers\AppSection\User\Tasks\GetAllUsersTask;
 use App\Ship\Parents\Actions\Action;
+use Prettus\Repository\Exceptions\RepositoryException;
 
 class GetAllUsersAction extends Action
 {
+    /**
+     * @throws CoreInternalErrorException
+     * @throws RepositoryException
+     */
     public function run()
     {
         return app(GetAllUsersTask::class)->addRequestCriteria()->run();
