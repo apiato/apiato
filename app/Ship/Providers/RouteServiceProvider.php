4a5d4c45d088408ba9ed4b242a8afd9647af338c
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-25 14:58:43 +0330

    add comment

--- app/Ship/Providers/RouteServiceProvider.php
+++ app/Ship/Providers/RouteServiceProvider.php
@@ -6,7 +6,21 @@ use App\Ship\Parents\Providers\RouteServiceProvider as ParentRouteServiceProvide
 
 class RouteServiceProvider extends ParentRouteServiceProvider
 {
-    // Route names
+    /**
+     * The name of the web "home" route for your application.
+     *
+     * This is used by Apiato authentication to redirect users after login.
+     *
+     * @var string
+     */
     public const HOME = 'web_welcome_say_welcome';
+
+    /**
+     * The name of the web "login" route for your application.
+     *
+     * This is used by Apiato authentication to redirect users if unauthenticated.
+     *
+     * @var string
+     */
     public const LOGIN = 'login';
 }
