6956605ecc5c0f598d887566788a837e42a18159
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-28 17:27:19 +0330

    add test

--- app/Containers/AppSection/User/Tests/Unit/FindUserByIdTaskTest.php
+++ app/Containers/AppSection/User/Tests/Unit/FindUserByIdTaskTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Containers\AppSection\User\Tests\Unit;
 
+use App\Containers\AppSection\User\Models\User;
 use App\Containers\AppSection\User\Tasks\FindUserByIdTask;
 use App\Containers\AppSection\User\Tests\TestCase;
 use App\Ship\Exceptions\NotFoundException;
@@ -14,10 +15,18 @@ use App\Ship\Exceptions\NotFoundException;
  */
 class FindUserByIdTaskTest extends TestCase
 {
+    public function testFindUserById(): void
+    {
+        $user = User::factory()->create();
+
+        $foundUser = app(FindUserByIdTask::class)->run($user->id);
+
+        $this->assertEquals($user->id, $foundUser->id);
+    }
+
     public function testFindUserWithInvalidId(): void
     {
         $this->expectException(NotFoundException::class);
-        $this->expectExceptionMessage('The requested Resource was not found.');
 
         $noneExistingId = 777777;
 
