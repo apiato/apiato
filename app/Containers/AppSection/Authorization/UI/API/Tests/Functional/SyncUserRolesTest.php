a38f9221c63ec1ea6e5200b2fea4a44b373fd93c
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-28 19:35:38 +0330

    remove laravel validation and update tests

--- app/Containers/AppSection/Authorization/UI/API/Tests/Functional/SyncUserRolesTest.php
+++ app/Containers/AppSection/Authorization/UI/API/Tests/Functional/SyncUserRolesTest.php
@@ -5,7 +5,8 @@ namespace App\Containers\AppSection\Authorization\UI\API\Tests\Functional;
 use App\Containers\AppSection\Authorization\Models\Role;
 use App\Containers\AppSection\Authorization\UI\API\Tests\ApiTestCase;
 use App\Containers\AppSection\User\Models\User;
-use Illuminate\Support\Arr;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Vinkla\Hashids\Facades\Hashids;
 
 /**
  * Class SyncUserRolesTest.
@@ -24,25 +25,62 @@ class SyncUserRolesTest extends ApiTestCase
 
     public function testSyncMultipleRolesOnUser(): void
     {
-        $role1 = Role::factory()->create(['display_name' => '111']);
-        $role2 = Role::factory()->create(['display_name' => '222']);
-        $randomUser = User::factory()->create();
-        $randomUser->assignRole($role1);
+        $role1 = Role::factory()->create();
+        $role2 = Role::factory()->create();
+        $user = User::factory()->create();
+        $user->assignRole($role1);
         $data = [
             'roles_ids' => [
                 $role1->getHashedKey(),
                 $role2->getHashedKey(),
             ],
-            'user_id' => $randomUser->getHashedKey(),
+            'user_id' => $user->getHashedKey(),
         ];
 
         $response = $this->makeCall($data);
 
         $response->assertStatus(200);
-        $responseContent = $this->getResponseContentObject();
-        $this->assertTrue(count($responseContent->data->roles->data) > 1);
-        $roleIds = Arr::pluck($responseContent->data->roles->data, 'id');
-        $this->assertContains($data['roles_ids'][0], $roleIds);
-        $this->assertContains($data['roles_ids'][1], $roleIds);
+        $response->assertJson(
+            fn (AssertableJson $json) =>
+                $json->has('data')
+                    ->count('data.roles.data', 2)
+                    ->where('data.roles.data.0.id', $data['roles_ids'][0])
+                    ->where('data.roles.data.1.id', $data['roles_ids'][1])
+                    ->etc()
+        );
+    }
+
+    public function testSyncRoleOnNonExistingUser(): void
+    {
+        $role = Role::factory()->create();
+        $invalidId = 7777;
+        $data = [
+            'roles_ids' => [
+                $role->getHashedKey(),
+            ],
+            'user_id' => Hashids::encode($invalidId),
+        ];
+
+
+        $response = $this->makeCall($data);
+
+        $response->assertStatus(404);
+    }
+
+    public function testSyncNonExistingRoleOnUser(): void
+    {
+        $user = User::factory()->create();
+        $invalidId = 7777;
+        $data = [
+            'roles_ids' => [
+                Hashids::encode($invalidId),
+            ],
+            'user_id' => $user->getHashedKey(),
+        ];
+
+
+        $response = $this->makeCall($data);
+
+        $response->assertStatus(404);
     }
 }
