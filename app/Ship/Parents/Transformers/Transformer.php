d39dd050b93d11213fab560325717a6cd3f68e0a
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-29 20:41:34 +0330

    implement codes that moved form core to ship

--- app/Ship/Parents/Transformers/Transformer.php
+++ app/Ship/Parents/Transformers/Transformer.php
@@ -3,8 +3,24 @@
 namespace App\Ship\Parents\Transformers;
 
 use Apiato\Core\Abstracts\Transformers\Transformer as AbstractTransformer;
+use Illuminate\Support\Facades\Auth;
 
 abstract class Transformer extends AbstractTransformer
 {
+    /**
+     * @param $adminResponse
+     * @param $clientResponse
+     *
+     * @return  array
+     */
+    public function ifAdmin($adminResponse, $clientResponse): array
+    {
+        $user = Auth::user();
 
+        if (!is_null($user) && $user->hasAdminRole()) {
+            return array_merge($clientResponse, $adminResponse);
+        }
+
+        return $clientResponse;
+    }
 }
