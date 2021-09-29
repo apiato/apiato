d9e34cd8b946a6a741f12de16557e712181d1d5c
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-29 16:49:44 +0330

    remove db exists validation on request & update tests

--- app/Containers/AppSection/Authorization/UI/API/Tests/Functional/AttachPermissionsToRoleTest.php
+++ app/Containers/AppSection/Authorization/UI/API/Tests/Functional/AttachPermissionsToRoleTest.php
@@ -5,6 +5,8 @@ namespace App\Containers\AppSection\Authorization\UI\API\Tests\Functional;
 use App\Containers\AppSection\Authorization\Models\Permission;
 use App\Containers\AppSection\Authorization\Models\Role;
 use App\Containers\AppSection\Authorization\UI\API\Tests\ApiTestCase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Vinkla\Hashids\Facades\Hashids;
 
 /**
  * Class AttachPermissionsToRoleTest.
@@ -23,32 +25,79 @@ class AttachPermissionsToRoleTest extends ApiTestCase
 
     public function testAttachSinglePermissionToRole(): void
     {
-        $roleA = Role::factory()->create();
-        $permissionA = Permission::factory()->create();
+        $role = Role::factory()->create();
+        $permission = Permission::factory()->create();
         $data = [
-            'role_id' => $roleA->getHashedKey(),
-            'permissions_ids' => $permissionA->getHashedKey(),
+            'role_id' => $role->getHashedKey(),
+            'permissions_ids' => $permission->getHashedKey(),
         ];
 
         $response = $this->makeCall($data);
 
         $response->assertStatus(200);
-        $responseContent = $this->getResponseContentObject();
-        $this->assertEquals($roleA['name'], $responseContent->data->name);
+        $response->assertJson(
+            fn (AssertableJson $json) =>
+                $json->has('data')
+                    ->where('data.object', 'Role')
+                    ->where('data.id', $role->getHashedKey())
+                    ->has('data.permissions.data', 1)
+                    ->where('data.permissions.data.0.object', 'Permission')
+                    ->where('data.permissions.data.0.id', $permission->getHashedKey())
+                    ->etc()
+        );
     }
 
-    public function testAttachMultiplePermissionToRole(): void
+    public function testAttachMultiplePermissionsToRole(): void
     {
-        $roleA = Role::factory()->create();
+        $role = Role::factory()->create();
         $permissionA = Permission::factory()->create();
         $permissionB = Permission::factory()->create();
         $data = [
-            'role_id' => $roleA->getHashedKey(),
+            'role_id' => $role->getHashedKey(),
             'permissions_ids' => [$permissionA->getHashedKey(), $permissionB->getHashedKey()],
         ];
 
         $response = $this->makeCall($data);
 
         $response->assertStatus(200);
+        $response->assertJson(
+            fn (AssertableJson $json) =>
+            $json->has('data')
+                ->where('data.object', 'Role')
+                ->where('data.id', $role->getHashedKey())
+                ->has('data.permissions.data', 2)
+                ->where('data.permissions.data.0.object', 'Permission')
+                ->where('data.permissions.data.0.id', $permissionA->getHashedKey())
+                ->where('data.permissions.data.1.id', $permissionB->getHashedKey())
+                ->etc()
+        );
+    }
+
+    public function testAttachNonExistingPermissionToRole(): void
+    {
+        $role = Role::factory()->create();
+        $invalidId = 7777;
+        $data = [
+            'role_id' => $role->getHashedKey(),
+            'permissions_ids' => Hashids::encode($invalidId),
+        ];
+
+        $response = $this->makeCall($data);
+
+        $response->assertStatus(404);
+    }
+
+    public function testAttachPermissionToNonExistingRole(): void
+    {
+        $permission = Permission::factory()->create();
+        $invalidId = 7777;
+        $data = [
+            'role_id' => Hashids::encode($invalidId),
+            'permissions_ids' => $permission->getHashedKey(),
+        ];
+
+        $response = $this->makeCall($data);
+
+        $response->assertStatus(404);
     }
 }
