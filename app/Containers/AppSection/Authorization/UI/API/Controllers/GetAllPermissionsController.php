64981c53e8e8527f2944d43d6e97c04773f01a95
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-10-01 16:56:11 +0330

    add phpdocs

--- app/Containers/AppSection/Authorization/UI/API/Controllers/GetAllPermissionsController.php
+++ app/Containers/AppSection/Authorization/UI/API/Controllers/GetAllPermissionsController.php
@@ -2,21 +2,25 @@
 
 namespace App\Containers\AppSection\Authorization\UI\API\Controllers;
 
+use Apiato\Core\Exceptions\CoreInternalErrorException;
 use Apiato\Core\Exceptions\InvalidTransformerException;
 use App\Containers\AppSection\Authorization\Actions\GetAllPermissionsAction;
 use App\Containers\AppSection\Authorization\UI\API\Requests\GetAllPermissionsRequest;
 use App\Containers\AppSection\Authorization\UI\API\Transformers\PermissionTransformer;
 use App\Ship\Parents\Controllers\ApiController;
+use Prettus\Repository\Exceptions\RepositoryException;
 
 class GetAllPermissionsController extends ApiController
 {
     /**
      * @throws InvalidTransformerException
+     * @throws CoreInternalErrorException
+     * @throws RepositoryException
      */
     public function getAllPermissions(GetAllPermissionsRequest $request): array
     {
         $permissions = app(GetAllPermissionsAction::class)->run($request);
-        
+
         return $this->transform($permissions, PermissionTransformer::class);
     }
 }
--- app/Containers/AppSection/Authorization/UI/API/Controllers/GetAllRolesController.php
+++ app/Containers/AppSection/Authorization/UI/API/Controllers/GetAllRolesController.php
@@ -2,21 +2,25 @@
 
 namespace App\Containers\AppSection\Authorization\UI\API\Controllers;
 
+use Apiato\Core\Exceptions\CoreInternalErrorException;
 use Apiato\Core\Exceptions\InvalidTransformerException;
 use App\Containers\AppSection\Authorization\Actions\GetAllRolesAction;
 use App\Containers\AppSection\Authorization\UI\API\Requests\GetAllRolesRequest;
 use App\Containers\AppSection\Authorization\UI\API\Transformers\RoleTransformer;
 use App\Ship\Parents\Controllers\ApiController;
+use Prettus\Repository\Exceptions\RepositoryException;
 
 class GetAllRolesController extends ApiController
 {
     /**
      * @throws InvalidTransformerException
+     * @throws CoreInternalErrorException
+     * @throws RepositoryException
      */
     public function getAllRoles(GetAllRolesRequest $request): array
     {
         $roles = app(GetAllRolesAction::class)->run($request);
-        
+
         return $this->transform($roles, RoleTransformer::class);
     }
 }
