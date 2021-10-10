724ecf8ff56a03df0d375528236b674341524cc4
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-10-10 12:08:31 +0330

    fix redirect

--- app/Containers/AppSection/Authentication/UI/WEB/Controllers/LogoutController.php
+++ app/Containers/AppSection/Authentication/UI/WEB/Controllers/LogoutController.php
@@ -5,6 +5,7 @@ namespace App\Containers\AppSection\Authentication\UI\WEB\Controllers;
 use App\Containers\AppSection\Authentication\Actions\WebLogoutAction;
 use App\Containers\AppSection\Authentication\UI\WEB\Requests\LogoutRequest;
 use App\Ship\Parents\Controllers\WebController;
+use App\Ship\Providers\RouteServiceProvider;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Redirector;
@@ -14,6 +15,7 @@ class LogoutController extends WebController
     public function logout(LogoutRequest $request): Redirector|Application|RedirectResponse
     {
         app(WebLogoutAction::class)->run();
-        return redirect('/');
+
+        return redirect()->route(RouteServiceProvider::HOME);
     }
 }
