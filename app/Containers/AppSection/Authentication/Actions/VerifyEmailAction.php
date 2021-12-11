a9b7f7733cc76ed1005a8bea91ff03c254ab1c99
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2021-12-11 13:35:21 +0330

    Send EmailVerified email notification

--- app/Containers/AppSection/Authentication/Actions/VerifyEmailAction.php
+++ app/Containers/AppSection/Authentication/Actions/VerifyEmailAction.php
@@ -2,9 +2,9 @@
 
 namespace App\Containers\AppSection\Authentication\Actions;
 
+use App\Containers\AppSection\Authentication\Notifications\EmailVerified;
 use App\Containers\AppSection\Authentication\UI\API\Requests\VerifyEmailRequest;
 use App\Ship\Parents\Actions\Action;
-use Illuminate\Auth\Events\Verified;
 
 class VerifyEmailAction extends Action
 {
@@ -13,7 +13,7 @@ class VerifyEmailAction extends Action
         if (!$request->user()->hasVerifiedEmail()) {
             $request->user()->markEmailAsVerified();
 
-            event(new Verified($request->user()));
+            $request->user()->notify(new EmailVerified());
         }
     }
 }
