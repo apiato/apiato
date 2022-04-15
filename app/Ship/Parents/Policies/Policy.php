0e46ec4a08f00c5c43881e1e03c5df3bd09c8af6
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2022-04-15 12:07:24 +0430

    feat!: let admin access all routes through policy

--- app/Ship/Parents/Policies/Policy.php
+++ app/Ship/Parents/Policies/Policy.php
@@ -3,8 +3,24 @@
 namespace App\Ship\Parents\Policies;
 
 use Apiato\Core\Abstracts\Policies\Policy as AbstractPolicy;
+use App\Ship\Parents\Models\UserModel;
 
 abstract class Policy extends AbstractPolicy
 {
+    /**
+     * Perform pre-authorization checks.
+     *
+     * @param UserModel $user
+     * @param string $ability
+     * @return bool|null
+     */
+    public function before(UserModel $user, string $ability): ?bool
+    {
+        //grant access for admins
+        if (method_exists($user, 'hasAdminRole') && $user->hasAdminRole()) {
+            return true;
+        }
 
+        return null;
+    }
 }
