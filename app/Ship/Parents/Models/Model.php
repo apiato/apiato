8925d85722789f224dfa8a712d65b7ed13c37bda
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2022-05-02 10:12:27 +0430

    feat: use `CanOwn` trait

--- app/Ship/Parents/Models/Model.php
+++ app/Ship/Parents/Models/Model.php
@@ -3,8 +3,9 @@
 namespace App\Ship\Parents\Models;
 
 use Apiato\Core\Abstracts\Models\Model as AbstractModel;
+use Apiato\Core\Traits\CanOwnTrait;
 
 abstract class Model extends AbstractModel
 {
-
+    use CanOwnTrait;
 }
--- app/Ship/Parents/Models/UserModel.php
+++ app/Ship/Parents/Models/UserModel.php
@@ -3,6 +3,7 @@
 namespace App\Ship\Parents\Models;
 
 use Apiato\Core\Abstracts\Models\UserModel as AbstractUserModel;
+use Apiato\Core\Traits\CanOwnTrait;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
@@ -12,4 +13,5 @@ abstract class UserModel extends AbstractUserModel
     use Notifiable;
     use HasApiTokens;
     use HasRoles;
+    use CanOwnTrait;
 }
