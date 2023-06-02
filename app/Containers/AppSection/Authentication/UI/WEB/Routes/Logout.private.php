8567acc6a8d3a58f7dfbcb617dc1b78052cc7dd2
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-06-02 16:03:31 +0330

    refactor: update docs
    Update web route names to be consistence with API route naming conventions

--- app/Containers/AppSection/Authentication/UI/WEB/Routes/Logout.private.php
+++ app/Containers/AppSection/Authentication/UI/WEB/Routes/Logout.private.php
@@ -3,5 +3,5 @@
 use App\Containers\AppSection\Authentication\UI\WEB\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/logout', [LogoutController::class, 'logout'])
+Route::post('web/logout', [LogoutController::class, 'logout'])
     ->name('post_logout');
