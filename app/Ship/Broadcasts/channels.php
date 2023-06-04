73a8acb20c27ddc8835c1e16a681f78a3c5752eb
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-06-04 14:52:46 +0330

    update: usage

--- app/Ship/Broadcasts/channels.php
+++ app/Ship/Broadcasts/channels.php
@@ -11,6 +11,6 @@
 |
 */
 
-//Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//Illuminate\Support\Facades\Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 //    return (int) $user->id === (int) $id;
 //});
