78ed79bcbe407b8b66348d9a5cf6649d25b365d7
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-06-04 14:52:27 +0330

    update: comments

--- app/Ship/Parents/Providers/MainServiceProvider.php
+++ app/Ship/Parents/Providers/MainServiceProvider.php
@@ -4,6 +4,11 @@ namespace App\Ship\Parents\Providers;
 
 use Apiato\Core\Abstracts\Providers\MainServiceProvider as AbstractMainServiceProvider;
 
+/**
+ * Class MainServiceProvider
+ *
+ * A.K.A. app/Providers/AppServiceProvider.php
+ */
 abstract class MainServiceProvider extends AbstractMainServiceProvider
 {
     /**
