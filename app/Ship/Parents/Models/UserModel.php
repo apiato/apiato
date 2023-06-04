76b4f299112aecf7cc8d85a8e17a2e774f2142dc
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-06-04 20:58:06 +0330

    feat: remove trait
    This trait is now added in the Core

--- app/Ship/Parents/Models/UserModel.php
+++ app/Ship/Parents/Models/UserModel.php
@@ -3,7 +3,6 @@
 namespace App\Ship\Parents\Models;
 
 use Apiato\Core\Abstracts\Models\UserModel as AbstractUserModel;
-use Apiato\Core\Traits\CanOwnTrait;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
@@ -13,5 +12,4 @@ abstract class UserModel extends AbstractUserModel
     use Notifiable;
     use HasApiTokens;
     use HasRoles;
-    use CanOwnTrait;
 }
