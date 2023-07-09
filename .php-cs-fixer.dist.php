0a8d9e3d90b33aa3500d146949151162b5ee5c12
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-07-09 16:48:38 +0330

    style: change `php cs fixer`  configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,43 +1,23 @@
 <?php
 
-declare(strict_types=1);
+use PhpCsFixer\Finder;
 
-$finder = PhpCsFixer\Finder::create()
+$finder = Finder::create()
     ->in([
         __DIR__ . '/app',
     ])
-    ->name('*.php')
     ->notName('*.blade.php')
-    ->ignoreDotFiles(true)
-    ->exclude('vendor')
-    ->exclude('/app/Containers/Vendor')
-    ->ignoreVCS(true)
-    ->append(array('.php-cs-fixer.dist.php'));
+    ->exclude('Containers/Vendor');
 
-$config = new PhpCsFixer\Config();
-return $config->setRules([
-    '@PSR12' => true,
-    'array_syntax' => ['syntax' => 'short'],
-    'ordered_imports' => ['sort_algorithm' => 'alpha'],
-    'no_unused_imports' => true,
-    'trailing_comma_in_multiline' => true,
-    'phpdoc_scalar' => true,
-    'unary_operator_spaces' => true,
-    'binary_operator_spaces' => true,
-    'blank_line_before_statement' => [
-        'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
-    ],
-    'phpdoc_single_line_var_spacing' => true,
-    'phpdoc_var_without_name' => true,
-    'class_attributes_separation' => [
-        'elements' => [
-            'method' => 'one',
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'method_argument_space' => [
+            'on_multiline' => 'ensure_fully_multiline',
         ],
-    ],
-    'method_argument_space' => [
-        'on_multiline' => 'ensure_fully_multiline',
-        'keep_multiple_spaces_after_comma' => true,
-    ],
-    'single_trait_insert_per_statement' => true,
-])->setRiskyAllowed(true)
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+    ])
     ->setFinder($finder);
