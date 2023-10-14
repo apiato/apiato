11eb69a42f7d75bce615c5070a848fe5563fca97
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-10-14 21:46:13 +0330

    feat: generate `verify email` route only if email verification is enabled

--- app/Containers/AppSection/Authentication/UI/API/Routes/VerifyEmail.v1.private.php
+++ app/Containers/AppSection/Authentication/UI/API/Routes/VerifyEmail.v1.private.php
@@ -33,6 +33,8 @@
 use App\Containers\AppSection\Authentication\UI\API\Controllers\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('email/verify/{id}/{hash}', VerifyEmailController::class)
+if (config('appSection-authentication.require_email_verification')) {
+    Route::post('email/verify/{id}/{hash}', VerifyEmailController::class)
     ->name('verification.verify')
     ->middleware('signed');
+}
