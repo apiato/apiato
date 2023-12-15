6a105bd430a3710f6267e3cc5e6e68f6f6588f03
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-12-15 04:42:53 +0330

    refactor: remove not necessary implementation
    ShouldQueue contract is already implemented by Core

--- app/Ship/Parents/Jobs/Job.php
+++ app/Ship/Parents/Jobs/Job.php
@@ -3,23 +3,7 @@
 namespace App\Ship\Parents\Jobs;
 
 use Apiato\Core\Abstracts\Jobs\Job as AbstractJob;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
-/**
- * Class Job.
- *
- * A.K.A. (app/Jobs/Job.php)
- */
-abstract class Job extends AbstractJob implements ShouldQueue
+abstract class Job extends AbstractJob
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Queueable Jobs
-    |--------------------------------------------------------------------------
-    |
-    | This job base class provides a central location to place any logic that
-    | is shared across all of your jobs. The trait included with the class
-    | provides access to the "onQueue" and "delay" queue helper methods.
-    |
-    */
 }
