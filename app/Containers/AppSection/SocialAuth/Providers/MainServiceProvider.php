54a17222fcd4bc3c1e61827b67df6f368664b0e1
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-04 19:50:45 +0330

    docs: remove php docs

--- app/Containers/AppSection/SocialAuth/Providers/MainServiceProvider.php
+++ app/Containers/AppSection/SocialAuth/Providers/MainServiceProvider.php
@@ -4,33 +4,14 @@ namespace App\Containers\AppSection\SocialAuth\Providers;
 
 use Apiato\Core\Abstracts\Providers\MainServiceProvider as ParentServiceProvider;
 
-/**
- * Class MainServiceProvider.
- *
- * The Main Service Provider of this container, it will be automatically registered in the framework.
- *
- * @author  Mahmoud Zalt <mahmoud@zalt.me>
- */
 final class MainServiceProvider extends ParentServiceProvider
 {
-    /**
-     * Container Service Providers.
-     */
-    public array $serviceProviders = [
-    ];
+    public array $serviceProviders = [];
 
-    /**
-     * Container Aliases.
-     */
-    public array $aliases = [
-    ];
+    public array $aliases = [];
 
-    /**
-     * Register anything in the container.
-     */
     public function register(): void
     {
         parent::register();
-        // do your binding here..
     }
 }
