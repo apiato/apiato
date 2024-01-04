99650a3c14de8313d38552e31c996200e79696ea
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-04 19:50:11 +0330

    docs: remove php docs

--- app/Containers/AppSection/SocialAuth/UI/API/Requests/ApiAuthenticateRequest.php
+++ app/Containers/AppSection/SocialAuth/UI/API/Requests/ApiAuthenticateRequest.php
@@ -6,31 +6,18 @@ use Apiato\Core\Abstracts\Requests\Request;
 
 final class ApiAuthenticateRequest extends Request
 {
-    /**
-     * Define which Roles and/or Permissions has access to this request.
-     */
     protected array $access = [
         'permissions' => '',
         'roles' => '',
     ];
 
-    /**
-     * Id's that needs decoding before applying the validation rules.
-     */
     protected array $decode = [
     ];
 
-    /**
-     * Defining the URL parameters (`/stores/999/items`) allows applying
-     * validation rules on them and allows accessing them like request data.
-     */
     protected array $urlParameters = [
         'provider',
     ];
 
-    /**
-     * Get the validation rules that apply to the request.
-     */
     public function rules(): array
     {
         return [
@@ -38,9 +25,6 @@ final class ApiAuthenticateRequest extends Request
         ];
     }
 
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return $this->check([
