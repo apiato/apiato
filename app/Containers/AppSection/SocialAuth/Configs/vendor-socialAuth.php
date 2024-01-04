8c64bd3dd0be962ce5469398654f0a64a3b27c3f
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-04 19:57:35 +0330

    Update vendor-socialAuth.php

--- app/Containers/AppSection/SocialAuth/Configs/vendor-socialAuth.php
+++ app/Containers/AppSection/SocialAuth/Configs/vendor-socialAuth.php
@@ -2,9 +2,11 @@
 
 return [
     /*
-     * This container already provides 3 provider. You can implement and add your SocialAuthProvider here.
+     * This container already provides 3 providers.
+     * You can implement and add your SocialAuthProvider here.
      *
      * The class you want to use as a SocialAuthProvider needs to implement the
+     * TODO: fix namespace after moving to its own package
      * `App\Containers\AppSection\SocialAuth\Contracts\SocialAuthProvider` contract.
      */
     'providers' => [
@@ -14,8 +16,8 @@ return [
     ],
 
     /*
-     * Social Authentication container depends on Apiato's default user repository and transformer but
-     * if your user repository or transformer is different from Apiato's default, you can provide them here
+     * Social Authentication container depends on Apiato's default user repository and transformer, but
+     * if your user repository or transformer is different from Apiato's default, you can provide them here.
      */
     'user' => [
         'repository' => App\Containers\AppSection\User\Data\Repositories\UserRepository::class,
