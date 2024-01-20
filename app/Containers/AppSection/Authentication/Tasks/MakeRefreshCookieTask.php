7174402ff90498a81b319faebb012da666f0a7d9
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-20 18:49:14 +0330

    Update MakeRefreshCookieTask.php

--- app/Containers/AppSection/Authentication/Tasks/MakeRefreshCookieTask.php
+++ app/Containers/AppSection/Authentication/Tasks/MakeRefreshCookieTask.php
@@ -3,12 +3,11 @@
 namespace App\Containers\AppSection\Authentication\Tasks;
 
 use App\Ship\Parents\Tasks\Task as ParentTask;
-use Illuminate\Cookie\CookieJar;
 use Symfony\Component\HttpFoundation\Cookie;
 
 class MakeRefreshCookieTask extends ParentTask
 {
-    public function run(string $refreshToken): CookieJar|Cookie
+    public function run(string $refreshToken): Cookie
     {
         return cookie(
             'refreshToken',
