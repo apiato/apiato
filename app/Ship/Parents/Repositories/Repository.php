a58cf21a5dad71156598b300a7731f6b53caef70
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-20 21:26:51 +0330

    Update Repository.php

--- app/Ship/Parents/Repositories/Repository.php
+++ app/Ship/Parents/Repositories/Repository.php
@@ -3,7 +3,35 @@
 namespace App\Ship\Parents\Repositories;
 
 use Apiato\Core\Abstracts\Repositories\Repository as AbstractRepository;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Prettus\Repository\Contracts\CriteriaInterface;
+use Prettus\Repository\Exceptions\RepositoryException;
 
 abstract class Repository extends AbstractRepository
 {
+    public function delete($id): bool|null
+    {
+        $result = parent::delete($id);
+
+        if (null === $result) {
+            return null;
+        }
+
+        return (bool) $result;
+    }
+
+    /**
+     * @param class-string<CriteriaInterface> $criteria Criteria class name
+     * @param array<string, mixed> $args Arguments to pass to the criteria constructor
+     *
+     * @throws RepositoryException
+     * @throws BindingResolutionException
+     */
+    public function pushCriteriaWith(string $criteria, array $args): static
+    {
+        /** @var CriteriaInterface $criteriaInstance */
+        $criteriaInstance = $this->app->makeWith($criteria, $args);
+
+        return $this->pushCriteria($criteriaInstance);
+    }
 }
