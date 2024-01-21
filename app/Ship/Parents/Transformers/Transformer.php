2423d9bf93bd33381d92d6fe0661b57d38c49e33
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-21 15:28:13 +0330

    feat(API-533): remove `isAdmin` method
    https://apiato.atlassian.net/browse/API-533

--- app/Ship/Parents/Transformers/Transformer.php
+++ app/Ship/Parents/Transformers/Transformer.php
@@ -3,18 +3,7 @@
 namespace App\Ship\Parents\Transformers;
 
 use Apiato\Core\Abstracts\Transformers\Transformer as AbstractTransformer;
-use Illuminate\Support\Facades\Auth;
 
 abstract class Transformer extends AbstractTransformer
 {
-    public function ifAdmin(array $adminResponse, array $clientResponse): array
-    {
-        $user = Auth::user();
-
-        if (!is_null($user) && $user->hasAdminRole()) {
-            return array_merge($clientResponse, $adminResponse);
-        }
-
-        return $clientResponse;
-    }
 }
