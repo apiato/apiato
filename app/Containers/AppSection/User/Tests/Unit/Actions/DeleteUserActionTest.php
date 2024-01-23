b500fff7470eb269b83497a3b2b56c35f6cf55ca
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-23 15:20:28 +0330

    Update DeleteUserActionTest.php

--- app/Containers/AppSection/User/Tests/Unit/Actions/DeleteUserActionTest.php
+++ app/Containers/AppSection/User/Tests/Unit/Actions/DeleteUserActionTest.php
@@ -4,8 +4,10 @@ namespace App\Containers\AppSection\User\Tests\Unit\Actions;
 
 use App\Containers\AppSection\User\Actions\DeleteUserAction;
 use App\Containers\AppSection\User\Data\Factories\UserFactory;
+use App\Containers\AppSection\User\Data\Repositories\UserRepository;
 use App\Containers\AppSection\User\Tests\UnitTestCase;
 use App\Containers\AppSection\User\UI\API\Requests\DeleteUserRequest;
+use App\Ship\Exceptions\DeleteResourceFailedException;
 use App\Ship\Exceptions\NotFoundException;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
@@ -32,6 +34,18 @@ final class DeleteUserActionTest extends UnitTestCase
         $nonExistingID = 7777777;
         $request = DeleteUserRequest::injectData()->withUrlParameters(['id' => $nonExistingID]);
         $action = app(DeleteUserAction::class);
+
+        $action->run($request);
+    }
+
+    public function testCatchesAllExceptionsAndThrowsDeleteResourceFailedException(): void
+    {
+        $this->expectException(DeleteResourceFailedException::class);
+
+        $request = DeleteUserRequest::injectData();
+        $mockRepository = $this->mock(UserRepository::class);
+        $mockRepository->allows()->delete()->andThrow(new \Exception());
+        $action = new DeleteUserAction($mockRepository);
 
         $action->run($request);
     }
