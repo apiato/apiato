ff1d4a4ee934a582afd8a47caa59cbadad82aa26
Igor Danilov <polarikus@yandex.ru>
2024-01-25 18:41:29 +0300

    Update LoginControllerTest.php

--- app/Containers/AppSection/Authentication/Tests/Unit/UI/WEB/Controllers/LoginControllerTest.php
+++ app/Containers/AppSection/Authentication/Tests/Unit/UI/WEB/Controllers/LoginControllerTest.php
@@ -6,6 +6,8 @@ use App\Containers\AppSection\Authentication\Actions\WebLoginAction;
 use App\Containers\AppSection\Authentication\Tests\UnitTestCase;
 use App\Containers\AppSection\Authentication\UI\WEB\Controllers\LoginController;
 use App\Containers\AppSection\Authentication\UI\WEB\Requests\LoginRequest;
+use App\Ship\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
 
@@ -21,5 +23,20 @@ final class LoginControllerTest extends UnitTestCase
         $actionMock->expects()->run($request);
 
         $controller->__invoke($request, $actionMock);
+    }
+
+    public function testControllerRedirectWithException(): void
+    {
+        $exceptionMessage = 'Test Exception';
+
+        $controller = app(LoginController::class);
+        $request = LoginRequest::injectData();
+        $actionMock = $this->mock(WebLoginAction::class);
+        $actionMock->shouldReceive('run')->andThrowExceptions([new \Exception($exceptionMessage)]);
+
+        $response = $controller->__invoke($request, $actionMock);
+
+        $this->assertTrue($response->isRedirect(route(RouteServiceProvider::LOGIN)));
+        $this->assertSame($exceptionMessage, $response->getSession()->get('login'));
     }
 }
