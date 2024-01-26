a96b5029d0d96e88576b4ac118c2cb10d42c5fd1
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-26 21:06:17 +0330

    Update PermissionFactoryTest.php

--- app/Containers/AppSection/Authorization/Tests/Unit/Data/Factories/PermissionFactoryTest.php
+++ app/Containers/AppSection/Authorization/Tests/Unit/Data/Factories/PermissionFactoryTest.php
@@ -5,6 +5,7 @@ namespace App\Containers\AppSection\Authorization\Tests\Unit\Data\Factories;
 use App\Containers\AppSection\Authorization\Data\Factories\PermissionFactory;
 use App\Containers\AppSection\Authorization\Models\Permission;
 use App\Containers\AppSection\Authorization\Tests\UnitTestCase;
+use App\Ship\Enums\AuthGuard;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
 
@@ -12,10 +13,17 @@ use PHPUnit\Framework\Attributes\Group;
 #[CoversClass(PermissionFactory::class)]
 final class PermissionFactoryTest extends UnitTestCase
 {
-    public function testCreatePermission(): void
+    public function testCanCreatePermission(): void
     {
         $permission = PermissionFactory::new()->createOne();
 
         $this->assertInstanceOf(Permission::class, $permission);
+    }
+
+    public function testCanSetGuard(): void
+    {
+        $permission = PermissionFactory::new()->withGuard(AuthGuard::API->value)->createOne();
+
+        $this->assertSame(AuthGuard::API->value, $permission->guard_name);
     }
 }
