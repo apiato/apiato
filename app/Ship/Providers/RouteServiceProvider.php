f44c72b91c75cc9d4024a2a5926f147151bfb0bd
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-26 17:13:07 +0330

    Update RouteServiceProvider.php

--- app/Ship/Providers/RouteServiceProvider.php
+++ app/Ship/Providers/RouteServiceProvider.php
@@ -6,30 +6,4 @@ use App\Ship\Parents\Providers\RouteServiceProvider as ParentRouteServiceProvide
 
 class RouteServiceProvider extends ParentRouteServiceProvider
 {
-    /**
-     * The name of the web "home" route for your application.
-     *
-     * This is used by Apiato authentication to redirect users after login.
-     *
-     * @var string
-     */
-    public const HOME = 'web_welcome_say_welcome';
-
-    /**
-     * The name of the web "login" route for your application.
-     *
-     * This is used by Apiato authentication to redirect users if unauthenticated.
-     *
-     * @var string
-     */
-    public const LOGIN = 'login';
-
-    /**
-     * The name of the web "unauthorized" route for your application.
-     *
-     * This is used by Apiato authentication to redirect users if unauthorized.
-     *
-     * @var string
-     */
-    public const UNAUTHORIZED = 'unauthorized';
 }
