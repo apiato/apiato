52e240db7a2df58b84b40b9df7842ba2adf6fe3a
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-27 21:05:25 +0330

    Update 2021_03_01_150940_create_jobs_table.php

--- app/Ship/Migrations/2021_03_01_150940_create_jobs_table.php
+++ app/Ship/Migrations/2021_03_01_150940_create_jobs_table.php
@@ -7,18 +7,15 @@ use Illuminate\Support\Facades\Schema;
 return new class() extends Migration {
     public function up(): void
     {
-        if ('database' === Config::get('queue.default')) {
-            Schema::create('jobs', function (Blueprint $table) {
-                $table->id();
-                $table->string('queue')->index();
-                $table->longText('payload');
-                $table->unsignedTinyInteger('attempts');
-                $table->unsignedInteger('reserved_at')->nullable();
-                $table->unsignedInteger('available_at');
-                $table->unsignedInteger('created_at');
-                $table->index(['queue', 'reserved_at']);
-            });
-        }
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
     }
 
     public function down(): void
