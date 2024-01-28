704cde23fc0a75057b0c5c8aa74fd7bbdafb589f
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-28 20:43:10 +0330

    Update Redirect.private.php

--- app/Containers/AppSection/SocialAuth/UI/WEB/Routes/Redirect.private.php
+++ app/Containers/AppSection/SocialAuth/UI/WEB/Routes/Redirect.private.php
@@ -3,5 +3,4 @@
 use App\Containers\AppSection\SocialAuth\UI\API\Controllers\RedirectController;
 use Illuminate\Support\Facades\Route;
 
-// Useless for now. I have no idea how this can be used. Maybe for a mobile app?
 Route::get('social-auth/redirect/{provider}', RedirectController::class);
