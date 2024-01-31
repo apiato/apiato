c4220426741118301120baa2e64179e252011c95
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-01-31 21:05:35 +0330

    Update vendor-socialAuth.php

--- app/Containers/AppSection/SocialAuth/Configs/vendor-socialAuth.php
+++ app/Containers/AppSection/SocialAuth/Configs/vendor-socialAuth.php
@@ -9,7 +9,6 @@ return [
      * if your user repository or transformer is different from Apiato's default, you can provide them here.
      */
     'user' => [
-        'user_model' => App\Containers\AppSection\User\Models\User::class,
         'repository' => App\Containers\AppSection\User\Data\Repositories\UserRepository::class,
         'transformer' => App\Containers\AppSection\User\UI\API\Transformers\UserTransformer::class,
         'table_name' => 'oauth_identities',
