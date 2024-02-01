ccf4d0daeca3016e6c3e7c631fc82dcdabfbcab4
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-01 18:19:29 +0330

    refactor: remove unused configs

--- app/Containers/AppSection/SocialAuth/Configs/vendor-socialAuth.php
+++ app/Containers/AppSection/SocialAuth/Configs/vendor-socialAuth.php
@@ -5,23 +5,17 @@ return [
     'can_link_multiple_providers' => false,
     'can_unlink_providers' => false,
     'can_signup_without_email' => false,
+
     /*
     |--------------------------------------------------------------------------
-    | Auto Verify Email
+    | User Configs
     |--------------------------------------------------------------------------
     |
-    | While authenticating with a social provider, if social email matches an existing user email, then we will
-    | verify user email automatically.
-    | User must implement Illuminate\Contracts\Auth\MustVerifyEmail interface.
+    | Social Authentication container depends on Apiato's default user repository and transformer, but
+    | if your user repository or transformer is different from Apiato's default, you can provide them here.
     |
     */
 
-    'auto_verify_email' => true,
-
-    /*
-     * Social Authentication container depends on Apiato's default user repository and transformer, but
-     * if your user repository or transformer is different from Apiato's default, you can provide them here.
-     */
     'user' => [
         'table_name' => 'oauth_identities',
     ],
