d26ee0150c6256fecf6db50825211565f8fadb7d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-01 18:19:08 +0330

    refactor: rename

--- app/Containers/AppSection/SocialAuth/Exceptions/OAuthIdentityLinkingException.php
+++ app/Containers/AppSection/SocialAuth/Exceptions/OAuthIdentityLinkingException.php
@@ -5,7 +5,7 @@ namespace App\Containers\AppSection\SocialAuth\Exceptions;
 use Apiato\Core\Abstracts\Exceptions\Exception;
 use Symfony\Component\HttpFoundation\Response;
 
-final class OAuthIdentityAlreadyLinkedException extends Exception
+final class OAuthIdentityLinkingException extends Exception
 {
     protected $code = Response::HTTP_CONFLICT;
     protected $message = 'OAuth identity is already linked to another user';
