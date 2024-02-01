207d56ef3fc1695a1f2441b2ee5008f1daa10d8a
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-01 18:19:37 +0330

    Update UnlinkOAuthIdentityAction.php

--- app/Containers/AppSection/SocialAuth/Actions/UnlinkOAuthIdentityAction.php
+++ app/Containers/AppSection/SocialAuth/Actions/UnlinkOAuthIdentityAction.php
@@ -3,58 +3,31 @@
 namespace App\Containers\AppSection\SocialAuth\Actions;
 
 use Apiato\Core\Abstracts\Actions\Action;
-use Apiato\Core\Exceptions\AuthenticationException;
-use App\Containers\AppSection\SocialAuth\Data\Repositories\OAuthIdentityRepository;
-use App\Containers\AppSection\SocialAuth\Models\OAuthIdentity;
+use App\Containers\AppSection\SocialAuth\Exceptions\OAuthIdentityLinkingException;
+use App\Containers\AppSection\SocialAuth\Exceptions\OAuthIdentityNotFoundException;
 use App\Containers\AppSection\SocialAuth\Tasks\FindOAuthIdentityTask;
-use Illuminate\Auth\AuthManager;
-use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Model;
 
 final class UnlinkOAuthIdentityAction extends Action
 {
-    private readonly Authenticatable&MustVerifyEmail $user;
-
-    /**
-     * @throws AuthenticationException
-     * @throws \Exception
-     */
     public function __construct(
         private readonly FindOAuthIdentityTask $findOAuthIdentityTask,
-        private readonly AuthManager $authManager,
-        private readonly OAuthIdentityRepository $oAuthIdentityRepository,
     ) {
-        if ($this->authManager->guest()) {
-            throw new AuthenticationException('User is not authenticated.');
-        }
-
-        if (!$this->authManager->user() instanceof MustVerifyEmail) {
-            // TODO: Throw a more specific exception.
-            throw new \Exception('User must implement Illuminate\Contracts\Auth\MustVerifyEmail\MustVerifyEmail.');
-        }
-
-        $this->user = $this->authManager->user();
     }
 
     /**
-     * @throws \Exception
+     * @throws OAuthIdentityNotFoundException
+     * @throws OAuthIdentityLinkingException
      */
-    public function run(string $provider, string $socialId): void
+    public function run(Model&MustVerifyEmail $user, string $provider, string $socialId): void
     {
         $identity = $this->findOAuthIdentityTask->run($provider, $socialId);
 
-        if ($identity->user->id !== $this->user->id) {
-            // TODO: Throw a more specific exception.
-            throw new \Exception('Cannot unlink OAuth identity from another user.');
+        if ($identity->user->isNot($user)) {
+            throw new OAuthIdentityLinkingException('This account is not linked to this user.');
         }
 
-        $this->unlinkIdentityFromToUser($identity);
-        $this->oAuthIdentityRepository->delete($identity->id);
-    }
-
-    private function unlinkIdentityFromToUser(OAuthIdentity $identity): void
-    {
-        $identity->user()->disassociate();
-        $identity->save();
+        $identity->unlinkUser();
     }
 }
