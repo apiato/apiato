ac39d06334c3278d47aa460066b83ea3dc06c1c0
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-01 18:19:56 +0330

    Update StatefulSignupByCodeAction.php

--- app/Containers/AppSection/SocialAuth/Actions/StatefulSignupByCodeAction.php
+++ app/Containers/AppSection/SocialAuth/Actions/StatefulSignupByCodeAction.php
@@ -3,12 +3,7 @@
 namespace App\Containers\AppSection\SocialAuth\Actions;
 
 use Apiato\Core\Abstracts\Actions\Action;
-use Apiato\Core\Abstracts\Models\UserModel;
-use App\Containers\AppSection\SocialAuth\Exceptions\OAuthIdentityNotFoundException;
-use App\Containers\AppSection\SocialAuth\Models\OAuthIdentity;
-use App\Containers\AppSection\SocialAuth\Tasks\FindOAuthIdentityTask;
 use App\Containers\AppSection\SocialAuth\Tasks\StatefulGetOAuthUserFromCodeTask;
-use App\Containers\AppSection\SocialAuth\Tasks\StoreOAuthIdentityTask;
 use App\Containers\AppSection\SocialAuth\Values\SocialAuthOutcome;
 use Prettus\Validator\Exceptions\ValidatorException;
 
@@ -16,46 +11,17 @@ final class StatefulSignupByCodeAction extends Action
 {
     public function __construct(
         private readonly StatefulGetOAuthUserFromCodeTask $statefulGetOAuthUserFromCodeTask,
-        private readonly FindOAuthIdentityTask $findOAuthIdentityTask,
-        private readonly StoreOAuthIdentityTask $storeOAuthIdentityTask,
+        private readonly SignupSubAction $signupSubAction,
     ) {
     }
 
     /**
      * @throws ValidatorException
-     * @throws \JsonException
      */
     public function run(string $provider): SocialAuthOutcome
     {
         $oAuthUser = $this->statefulGetOAuthUserFromCodeTask->run($provider);
 
-        try {
-            $identity = $this->findOAuthIdentityTask->run($provider, $oAuthUser->getId());
-        } catch (OAuthIdentityNotFoundException) {
-            $identity = $this->storeOAuthIdentityTask->run($provider, $oAuthUser);
-            // TODO: What if user is registered via a provider that doesn't provide email? e.g., Twitter or Facebook
-            //            if (!$oAuthUser->getEmail())
-            //                throw new \Exception('Email not provided by provider'
-            // TODO: What if the user email is not verified? (In provider).
-            // Can we assume that the user email is verified if the user is registered via a provider?
-            $user = $this->createVerifiedUser($identity, $oAuthUser->getEmail());
-            $this->linkIdentityToUser($identity, $user);
-        }
-
-        return new SocialAuthOutcome($identity);
-    }
-
-    private function createVerifiedUser(OAuthIdentity $identity, string $email): UserModel
-    {
-        $user = $identity->user()->create(compact('email'));
-        $user->markEmailAsVerified();
-
-        return $user;
-    }
-
-    private function linkIdentityToUser(OAuthIdentity $identity, UserModel $user): void
-    {
-        $identity->user()->associate($user);
-        $identity->save();
+        return $this->signupSubAction->run($provider, $oAuthUser);
     }
 }
