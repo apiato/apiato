6097c2c617a56b8f322754441d13ee92fa685c99
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-01 18:15:45 +0330

    fix: missing auth middleware

--- app/Containers/AppSection/SocialAuth/UI/API/Routes/LinkOAuthIdentity.v1.private.php
+++ app/Containers/AppSection/SocialAuth/UI/API/Routes/LinkOAuthIdentity.v1.private.php
@@ -3,8 +3,5 @@
 use App\Containers\AppSection\SocialAuth\UI\API\Controllers\LinkOAuthIdentityController;
 use Illuminate\Support\Facades\Route;
 
-// TODO: steps are incomplete
-// User has already logged in to the backend using email and password.
-// This endpoint is used to link a social account to an existing account
-// The backend tells the client app that the linking was successful.
-Route::post('social-auth/link/{provider}', LinkOAuthIdentityController::class);
+Route::post('social-auth/link/{provider}', LinkOAuthIdentityController::class)
+    ->middleware(['auth:api']);
