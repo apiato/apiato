89ccc009ec83898182e1ef5bd53b9da6ba547aea
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-01 10:23:51 +0330

    Update vendor-socialAuth.php

--- app/Containers/AppSection/SocialAuth/Configs/vendor-socialAuth.php
+++ app/Containers/AppSection/SocialAuth/Configs/vendor-socialAuth.php
@@ -6,6 +6,19 @@ return [
     'can_unlink_providers' => false,
     'can_signup_without_email' => false,
     /*
+    |--------------------------------------------------------------------------
+    | Auto Verify Email
+    |--------------------------------------------------------------------------
+    |
+    | While authenticating with a social provider, if social email matches an existing user email, then we will
+    | verify user email automatically.
+    | User must implement Illuminate\Contracts\Auth\MustVerifyEmail interface.
+    |
+    */
+
+    'auto_verify_email' => true,
+
+    /*
      * Social Authentication container depends on Apiato's default user repository and transformer, but
      * if your user repository or transformer is different from Apiato's default, you can provide them here.
      */
