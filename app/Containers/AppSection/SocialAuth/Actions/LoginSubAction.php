72fb18bce24188cb13753c4284893f221710d51e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-01 18:16:57 +0330

    feat: remove verify email operation

--- app/Containers/AppSection/SocialAuth/Actions/LoginSubAction.php
+++ app/Containers/AppSection/SocialAuth/Actions/LoginSubAction.php
@@ -6,7 +6,6 @@ use Apiato\Core\Abstracts\Actions\SubAction;
 use App\Containers\AppSection\SocialAuth\Exceptions\OAuthIdentityNotFoundException;
 use App\Containers\AppSection\SocialAuth\Tasks\FindOAuthIdentityTask;
 use App\Containers\AppSection\SocialAuth\Tasks\UpdateOAuthIdentityTask;
-use App\Containers\AppSection\SocialAuth\Tasks\VerifyEmailTask;
 use App\Containers\AppSection\SocialAuth\Values\SocialAuthOutcome;
 use Laravel\Socialite\Two\User;
 use Prettus\Validator\Exceptions\ValidatorException;
@@ -16,25 +15,17 @@ class LoginSubAction extends SubAction
     public function __construct(
         private readonly FindOAuthIdentityTask $findOAuthIdentityTask,
         private readonly UpdateOAuthIdentityTask $updateOAuthIdentityTask,
-        private readonly VerifyEmailTask $verifyEmailTask,
     ) {
     }
 
     /**
      * @throws OAuthIdentityNotFoundException
      * @throws ValidatorException
-     * @throws \JsonException
      */
     public function run(string $provider, User $oAuthUser): SocialAuthOutcome
     {
         $identity = $this->findOAuthIdentityTask->run($provider, $oAuthUser->getId());
-        $identity = $this->updateOAuthIdentityTask->run($identity->id, $oAuthUser);
-
-        // TODO: What if the user email is not verified? (In provider).
-        // Can we assume that the user email is verified if the user is registered via a provider?
-        if (null === $oAuthUser->getEmail()) {
-            $this->verifyEmailTask->run($identity->user, $oAuthUser->email);
-        }
+        $identity = $this->updateOAuthIdentityTask->run($identity->getKey(), $oAuthUser);
 
         return new SocialAuthOutcome($identity);
     }
