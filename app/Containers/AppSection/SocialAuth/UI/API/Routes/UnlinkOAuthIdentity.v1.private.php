34e6b8936f979b647f1ebd74061035d5d552a201
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-01 18:15:39 +0330

    fix: missing auth middleware

--- app/Containers/AppSection/SocialAuth/UI/API/Routes/UnlinkOAuthIdentity.v1.private.php
+++ app/Containers/AppSection/SocialAuth/UI/API/Routes/UnlinkOAuthIdentity.v1.private.php
@@ -3,4 +3,5 @@
 use App\Containers\AppSection\SocialAuth\UI\API\Controllers\UnlinkOAuthIdentityController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('social-auth/unlink/{provider}', UnlinkOAuthIdentityController::class);
+Route::post('social-auth/unlink/{provider}', UnlinkOAuthIdentityController::class)
+    ->middleware(['auth:api']);
