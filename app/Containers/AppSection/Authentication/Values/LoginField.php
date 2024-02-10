8f03781fcb44b225c6c80cafece5cfa596eafd5d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-10 14:24:28 +0330

    refactor

--- app/Containers/AppSection/Authentication/Values/LoginField.php
+++ app/Containers/AppSection/Authentication/Values/LoginField.php
@@ -3,13 +3,12 @@
 namespace App\Containers\AppSection\Authentication\Values;
 
 use App\Ship\Parents\Values\Value as ParentValue;
-use Illuminate\Validation\ValidationRuleParser;
 
 final class LoginField extends ParentValue implements \Stringable
 {
     public function __construct(
-        private string $name,
-        private array $rules,
+        private readonly string $name,
+        private readonly array $rules,
     ) {
     }
 
@@ -25,41 +24,11 @@ final class LoginField extends ParentValue implements \Stringable
 
     public function rules(): array
     {
-        return [$this->uniqueRules()];
+        return $this->rules;
     }
 
     public function name(): string
     {
         return $this->name;
-    }
-
-    private function uniqueRules(): string
-    {
-        ValidationRuleParser::parse($this->rules);
-
-        return implode('|', array_unique(explode('|', implode('|', $this->rules))));
-    }
-
-    private function isRequired(): bool
-    {
-        return !str_contains($this->uniqueRules(), 'required');
-    }
-
-    public function makeRequired(bool $required = true): self
-    {
-        if ($required && $this->isRequired()) {
-            $this->rules[] = 'required';
-        }
-
-        return $this;
-    }
-
-    private function trimPipes(): self
-    {
-        $instance = clone $this;
-
-        $instance->rules = [trim(str_replace('||', '|', $instance->rules), '|')];
-
-        return $instance;
     }
 }
