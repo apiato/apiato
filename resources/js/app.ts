c36d27dfbc422c4372ffff8209a65fb3b98e5d0e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-18 19:37:21 +0330

    feat: set app name dynamically

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,12 +1,17 @@
 import '../styles/sass/app.scss';
 import '@/utility';
+
 import type { App } from 'vue';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { registerPlugins } from '@/plugins';
 import { registerComponents, resolveComponent } from '@/components';
 
-createInertiaApp({
+void createInertiaApp({
+    title(title) {
+        const appName: string = import.meta.env.VITE_APP_TITLE || 'Apiato';
+        return title ? `${title} - ${appName}` : appName;
+    },
     resolve: resolveComponent,
     setup({ el, App, props, plugin }) {
         const app: App = createApp({ render: () => h(App, props) });
