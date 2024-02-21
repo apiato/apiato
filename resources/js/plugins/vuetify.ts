1ce694b1232ad8aac393ee641f253d525f6d0730
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-21 03:48:00 +0330

    Update vuetify.ts

--- resources/js/plugins/vuetify.ts
+++ resources/js/plugins/vuetify.ts
@@ -4,17 +4,22 @@ import { defaultLight } from '../themes/default-light.ts';
 import { defaultDark } from '../themes/default-dark.ts';
 import { md2 } from 'vuetify/blueprints';
 import '@mdi/font/css/materialdesignicons.css';
+import { mdi, aliases } from 'vuetify/iconsets/mdi';
 
 export default createVuetify({
-  blueprint: md2,
-  theme: {
-    defaultTheme: 'defaultDark',
-    themes: {
-      defaultDark,
-      defaultLight,
+    blueprint: md2,
+    theme: {
+        defaultTheme: 'defaultDark',
+        themes: {
+            defaultDark,
+            defaultLight,
+        },
     },
-  },
-  icons: {
-    defaultSet: 'mdi',
-  },
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
 });
