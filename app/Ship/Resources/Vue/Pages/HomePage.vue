2bd7902bb24737c804ef056e915cc0154db775a5
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-21 23:22:13 +0330

    Update HomePage.vue

--- app/Ship/Resources/Vue/Pages/HomePage.vue
+++ app/Ship/Resources/Vue/Pages/HomePage.vue
@@ -1,12 +1,38 @@
-<template>Home Page</template>
+<template>
+    <v-card>
+        <v-card-title>
+            <h3 class="text-h3">Users</h3>
+        </v-card-title>
+        <v-card-text>
+            <v-data-table :items="users" item-value="name">
+                <template #item="{ item }">
+                    <tr>
+                        <td v-for="field in userFields" :key="field">
+                            {{ item[field] }}
+                        </td>
+                    </tr>
+                </template>
+            </v-data-table>
+        </v-card-text>
+    </v-card>
+</template>
 
 <script setup lang="ts">
-import layout from '@ship/Vue/Layouts/DashboardLayout.vue';
-
-defineOptions({
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    layout,
+const props = defineProps({
+    users: {
+        type: Array,
+        required: true,
+    },
 });
+
+const headers = computed(() => [
+    // use user field keys as headers
+    ...Object.keys(props.users[0]).map((key) => ({
+        text: key,
+        value: key,
+    })),
+]);
+const userFields = computed(() => Object.keys(props.users[0]));
 </script>
 
 <style lang="scss"></style>
