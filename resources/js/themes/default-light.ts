07232015134dc01ffefa1934e614e01f59b20ebe
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-21 23:21:46 +0330

    Update default-light.ts

--- resources/js/themes/default-light.ts
+++ resources/js/themes/default-light.ts
@@ -1,5 +1,15 @@
 import { type ThemeDefinition } from 'vuetify';
+import colors from 'vuetify/util/colors';
 
 export const defaultLight: ThemeDefinition = {
-  dark: false,
+    dark: false,
+    colors: {
+        primary: colors.blue.darken2,
+        secondary: colors.grey.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+        info: colors.teal.lighten1,
+        success: colors.green.accent3,
+        warning: colors.amber.base,
+    },
 };
