168c77ee23f00890e4566b5eea591bf4556d073a
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-21 23:21:54 +0330

    Update DashboardLayout.vue

--- app/Ship/Resources/Vue/Layouts/DashboardLayout.vue
+++ app/Ship/Resources/Vue/Layouts/DashboardLayout.vue
@@ -1,11 +1,10 @@
 <template>
     <v-app id="the-app">
-        <v-layout class="rounded rounded-md">
+        <v-layout>
             <v-app-bar title="GESG Carbon Registry" />
             <TheNavigationDrawer />
             <v-main class="d-flex align-center justify-center" style="min-height: 300px">
                 <slot />
-                Main Content
             </v-main>
         </v-layout>
     </v-app>
