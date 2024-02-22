f4a1f858467f2b8b74b2afb8d88142ca8a056969
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-22 19:55:42 +0330

    Create IndexUsersPage.vue

---
+++ app/Containers/AppSection/User/UI/WEB/Pages/IndexUsersPage.vue
@@ -0,0 +1,145 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <h3 class="text-h3">Users</h3>
+        </v-card-title>
+        <v-card-text>
+            <v-data-table :on-update:options="options" :items="response.data" item-value="id">
+                <!--                <DataTableToolbar :title="dataTableTitle">-->
+                <!--                    <template #search>-->
+                <!--                        <DataTableSearchBox v-model:search-query="searchQuery" :loading="isSearching" />-->
+                <!--                    </template>-->
+                <!--                </DataTableToolbar>-->
+                <template #item="{ item }">
+                    <tr>
+                        <td v-for="field in userFields" :key="field">
+                            {{ item[field] }}
+                        </td>
+                    </tr>
+                </template>
+            </v-data-table>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import axios from 'axios';
+
+interface Pagination {
+    count: number;
+    current_page: number;
+    per_page: number;
+    total: number;
+    total_pages: number;
+}
+
+type Data = Record<string, unknown>;
+
+interface Response {
+    data: Data[];
+    meta: {
+        pagination: Pagination;
+    };
+}
+
+const props = defineProps({
+    response: {
+        type: Object as PropType<Response>,
+        required: true,
+    },
+});
+
+const options = reactive({
+    sortBy: [],
+    sortDesc: [false],
+    mustSort: true,
+    page: 1,
+    itemsPerPage: 10,
+});
+
+const items: Data[] = ref([]);
+const indexData = () =>
+    axios.get(route('index-users', {})).then((res: Response) => {
+        console.log(res);
+        items.values = res.items;
+        if (currentPage > res.meta.pagination.total_pages) {
+            currentPage = 1;
+        }
+    });
+
+onMounted(indexData);
+
+// const prepareGetAllURL = (payload: object, defaultUrl: string, includes: string | string[]) => {
+//     const params = new URLSearchParams();
+//
+//     if (payload.searchQuery) params.set('search', payload.searchQuery);
+//     if (payload.orderBy) params.set('orderBy', payload.orderBy);
+//     if (payload.sortedBy) params.set('sortedBy', payload.sortedBy);
+//     params.set('page', payload.currentPage ?? 1);
+//     params.set('limit', payload.perPage ?? 2000);
+//     if (payload.withIncludes && !!includes) {
+//         if (Array.isArray(includes)) {
+//             params.set('include', includes.join(','));
+//         } else {
+//             params.set('include', includes);
+//         }
+//     }
+//
+//     let url = `/${defaultUrl}?${params.toString()}`;
+//
+//     if (payload.additionalParams?.length) {
+//         payload.additionalParams.forEach((param) => {
+//             url += `&${param}`;
+//         });
+//     }
+//
+//     return url;
+// };
+
+// const FakeAPI = {
+//     async fetch({ page, itemsPerPage, sortBy }) {
+//         return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const start = (page - 1) * itemsPerPage;
+//                 const end = start + itemsPerPage;
+//                 const items = desserts.slice();
+//                 if (sortBy.length) {
+//                     const sortKey = sortBy[0].key;
+//                     const sortOrder = sortBy[0].order;
+//                     items.sort((a, b) => {
+//                         const aValue = a[sortKey];
+//                         const bValue = b[sortKey];
+//                         return sortOrder === 'desc' ? bValue - aValue : aValue - bValue;
+//                     });
+//                 }
+//                 const paginated = items.slice(start, end);
+//                 resolve({ items: paginated, total: items.length });
+//             }, 500);
+//         });
+//     },
+// };
+// const itemsPerPage = ref(5);
+// const search = ref('');
+// const serverItems = ref([]);
+// const loading = ref(true);
+// const totalItems = ref(0);
+// function loadItems({ page, itemsPerPage, sortBy }) {
+//     loading.value = true;
+//     FakeAPI.fetch({ page, itemsPerPage, sortBy }).then(({ items, total }) => {
+//         serverItems.value = items;
+//         totalItems.value = total;
+//         loading.value = false;
+//     });
+// }
+
+// const headers = computed(() => [
+//     // use user field keys as headers
+//     ...Object.keys(props.response.data[0]).map((key) => ({
+//         text: key,
+//         value: key,
+//     })),
+// ]);
+const userFields = computed(() => Object.keys(props.response.data[0]));
+</script>
+
+<style scoped lang="scss"></style>
