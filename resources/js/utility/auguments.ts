aba3b2bb8a9aa3d3e6f845e792ce06a7f6ee7806
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-22 19:53:28 +0330

    Update auguments.ts

--- resources/js/utility/auguments.ts
+++ resources/js/utility/auguments.ts
@@ -1,7 +1,6 @@
 export {};
 
 // https://www.typescriptlang.org/docs/handbook/declaration-files/templates/global-modifying-module-d-ts.html
-
 Object.defineProperty(String.prototype, 'camelToPascalCase', {
     value(): string {
         return this.split('/')
