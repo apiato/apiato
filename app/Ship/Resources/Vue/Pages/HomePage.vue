37f73d1598f190c8262def866828901b61667229
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-22 19:54:40 +0330

    Update HomePage.vue

--- app/Ship/Resources/Vue/Pages/HomePage.vue
+++ app/Ship/Resources/Vue/Pages/HomePage.vue
@@ -1,38 +1,7 @@
 <template>
-    <v-card>
-        <v-card-title>
-            <h3 class="text-h3">Users</h3>
-        </v-card-title>
-        <v-card-text>
-            <v-data-table :items="users" item-value="name">
-                <template #item="{ item }">
-                    <tr>
-                        <td v-for="field in userFields" :key="field">
-                            {{ item[field] }}
-                        </td>
-                    </tr>
-                </template>
-            </v-data-table>
-        </v-card-text>
-    </v-card>
+    <div>Home Page</div>
 </template>
 
-<script setup lang="ts">
-const props = defineProps({
-    users: {
-        type: Array,
-        required: true,
-    },
-});
-
-const headers = computed(() => [
-    // use user field keys as headers
-    ...Object.keys(props.users[0]).map((key) => ({
-        text: key,
-        value: key,
-    })),
-]);
-const userFields = computed(() => Object.keys(props.users[0]));
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss"></style>
