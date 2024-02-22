975c14ae105d3b4aeb6ddaf391f24064ad871c37
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-22 19:55:36 +0330

    Create IndexUsersController.php

---
+++ app/Containers/AppSection/User/UI/WEB/Controllers/IndexUsersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Containers\AppSection\User\UI\WEB\Controllers;
+
+use Apiato\Core\Traits\ResponseTrait;
+use App\Containers\AppSection\User\Actions\ListUsersAction;
+use App\Containers\AppSection\User\UI\API\Requests\ListUsersRequest;
+use App\Containers\AppSection\User\UI\API\Transformers\UserTransformer;
+use App\Ship\Parents\Controllers\WebController;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class IndexUsersController extends WebController
+{
+    use ResponseTrait;
+
+    public function __invoke(ListUsersRequest $request, ListUsersAction $action): Response
+    {
+        return Inertia::render('appSection@user::IndexUsersPage', [
+            'response' => fn (): array => $this->transform($action->run(), UserTransformer::class),
+        ]);
+    }
+}
