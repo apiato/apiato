ea48dc7aa8ed131ccb02cef96a23f2bcd81b27e3
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-23 12:15:21 +0330

    refactor: rename route

--- app/Containers/AppSection/User/UI/WEB/Routes/IndexUsers.php
+++ app/Containers/AppSection/User/UI/WEB/Routes/IndexUsers.php
@@ -4,4 +4,4 @@ use App\Containers\AppSection\User\UI\WEB\Controllers\IndexUsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users', IndexUsersController::class)
-    ->name('index-users');
+    ->name('users.index');
--- app/Ship/Resources/Vue/Components/TheNavigationDrawer.vue
+++ app/Ship/Resources/Vue/Components/TheNavigationDrawer.vue
@@ -24,7 +24,7 @@ const Dashboard: NavItem = {
 const User: NavItem = {
     title: 'Users',
     icon: 'mdi-account-search',
-    to: route('index-users'),
+    to: route('users.index'),
     children: [],
 };
 
