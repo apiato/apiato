035c87efac364fd3eaedf95f1af809760ebc61b0
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-23 18:39:29 +0330

    Update HandleInertiaRequests.php

--- app/Ship/Middlewares/HandleInertiaRequests.php
+++ app/Ship/Middlewares/HandleInertiaRequests.php
@@ -34,6 +34,13 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
+            'auth' => static fn () => [
+                'user' => $request->user() ? [
+                    'id' => $request->user()->id,
+                    'name' => $request->user()->name,
+                    'email' => $request->user()->email,
+                ] : null,
+            ],
         ]);
     }
 }
