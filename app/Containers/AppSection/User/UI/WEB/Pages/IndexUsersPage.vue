3db5a9757610a35dc14d89f89022fbd42fe74c50
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-25 18:14:31 +0330

    Update IndexUsersPage.vue

--- app/Containers/AppSection/User/UI/WEB/Pages/IndexUsersPage.vue
+++ app/Containers/AppSection/User/UI/WEB/Pages/IndexUsersPage.vue
@@ -5,7 +5,7 @@
 
             <v-spacer></v-spacer>
 
-            <v-text-field v-model="search" autofocus clearable label="Search" prepend-inner-icon="mdi-magnify" single-line variant="solo-filled" hide-details :loading="loading"></v-text-field>
+            <v-text-field v-model="search" clearable label="Search" prepend-inner-icon="mdi-magnify" single-line variant="solo-filled" hide-details :loading="loading"></v-text-field>
         </v-card-title>
 
         <v-divider />
@@ -30,32 +30,16 @@
 <script setup lang="ts">
 import type { PaginatedResponse } from '@ship/Js/Types/response';
 import type { UserContract } from '@containers/AppSection/User/UI/WEB/Contracts/user.ts';
+import type { ApiatoQueryString } from '@ship/Js/Types/shared-props';
 
 defineProps<PaginatedResponse<UserContract>>();
 const page = usePage<PaginatedResponse<UserContract>>();
 const users = computed(() => page.props.data);
+// const currentQuery = toRef(page.props.shared.query);
+const search = ref(page.props.shared.query.search ?? '');
 const pagination = page.props.meta.pagination;
 
 const loading = ref(false);
-const search = ref(page.props.shared.query.search);
-watch(search, (value) => {
-    if (value === '') {
-        const query = { ...page.props.shared.query };
-        delete query.search;
-        router.get(route('users.index'), query, {
-            onStart: () => {
-                loading.value = true;
-            },
-            preserveState: true,
-            replace: true,
-            onFinish: () => {
-                loading.value = false;
-            },
-        });
-    } else {
-        router.get(route('users.index'), { search: value }, { preserveState: true, replace: true });
-    }
-});
 // const headers = computed(() => Object.keys(page.props.data[0] ?? {}));
 const headers = ref([
     { title: 'Type', key: 'object', align: 'start' },
@@ -72,21 +56,11 @@ interface TableOptions {
     groupBy: string[];
     itemsPerPage: number;
     page: number;
-    search: string;
     sortBy: TableSortBy[];
-    // sortDesc: [false];
-    // mustSort: true;
+    search: undefined; // We use server side search so this is always undefined
 }
-
-function options($event: TableOptions) {
-    const query = { ...page.props.shared.query };
-    query.page = $event.page ?? 1;
-    query.limit = $event.itemsPerPage ?? 10;
-    if ($event.sortBy.length > 0) {
-        query.orderBy = $event.sortBy[0].key;
-        query.sortedBy = $event.sortBy[0].order;
-    }
-    query.search = $event.search;
+function applyQuery(query: ApiatoQueryString): void {
+    console.log(query);
     router.get(route('users.index'), query, {
         onStart: () => {
             loading.value = true;
@@ -98,6 +72,51 @@ function options($event: TableOptions) {
         },
     });
 }
+
+// type InvalidSearchType = undefined | null | '';
+// type ValidSearchType = string;
+
+function emptySearch(value: string | undefined | null): boolean {
+    return value === undefined || value === '' || value === null;
+}
+function options($event: TableOptions) {
+    let newQuery = { ...page.props.shared.query };
+
+    newQuery.page = $event.page ?? 1;
+    newQuery.limit = $event.itemsPerPage ?? 10;
+    if ($event.sortBy.length > 0 && $event.sortBy[0]) {
+        newQuery.orderBy = $event.sortBy[0].key;
+        newQuery.sortedBy = $event.sortBy[0].order;
+    }
+    if (!emptySearch(search.value)) {
+        newQuery = { ...newQuery, search: search.value };
+    }
+
+    applyQuery(newQuery);
+}
+
+watch(search, (newValue) => {
+    if (emptySearch(newValue)) {
+        const newQuery = { ...page.props.shared.query };
+
+        if (newQuery.search) delete newQuery.search;
+
+        applyQuery(newQuery);
+    } else {
+        const newQuery = { ...page.props.shared.query, search: newValue };
+
+        applyQuery(newQuery);
+    }
+});
+
+// interface PaginationParams {
+//     page: number;
+//     limit: number;
+//     orderBy: string;
+//     sortedBy: string;
+//     search: string;
+// }
+// interface QueryParams {}
 
 // const items: Data[] = ref([]);
 // const currentPage = ref(1);
