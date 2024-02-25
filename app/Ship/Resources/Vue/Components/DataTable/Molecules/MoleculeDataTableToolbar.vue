087fa1033322360813aa8c28241ba13bd0467415
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-25 23:13:48 +0330

    Create MoleculeDataTableToolbar.vue

---
+++ app/Ship/Resources/Vue/Components/DataTable/Molecules/MoleculeDataTableToolbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-card-title class="d-flex align-center pe-2">
+        {{ title }}
+        <v-spacer></v-spacer>
+        <v-text-field v-model="search" :disabled="loading" clearable label="Search" prepend-inner-icon="mdi-magnify" single-line variant="solo-filled" hide-details :loading="loading" />
+    </v-card-title>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    title: {
+        type: String,
+        default: '',
+    },
+    loading: {
+        type: Boolean,
+        default: false,
+    },
+});
+const search = defineModel('search', {
+    type: String,
+    required: true,
+});
+</script>
