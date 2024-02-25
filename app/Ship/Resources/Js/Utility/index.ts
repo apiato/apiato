9ccbe7f0d4f9c3a393e825d3325e1309712cfc08
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-02-26 03:04:15 +0330

    Update index.ts

--- app/Ship/Resources/Js/Utility/index.ts
+++ app/Ship/Resources/Js/Utility/index.ts
@@ -1,1 +1,9 @@
 import '@ship/Js/Utility/auguments.ts';
+
+export function debounce<F extends (...args: any[]) => any>(callback: F, delay = 300) {
+    let timeoutId: ReturnType<typeof setTimeout>;
+    return function (this: any, ...args: Parameters<F>) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => callback.apply(this, args), delay);
+    };
+}
