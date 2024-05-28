08071011043f2fc5c1272151dd1b0c260b7431ce
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-05-28 12:22:38 +0330

    feat: disable "Data" auto validation

--- app/Ship/Configs/data.php
+++ app/Ship/Configs/data.php
@@ -119,7 +119,7 @@ return [
      * method. By default, only when a request is passed the data is being validated. This
      * behaviour can be changed to always validate or to completely disable validation.
      */
-    'validation_strategy' => Spatie\LaravelData\Support\Creation\ValidationStrategy::OnlyRequests->value,
+    'validation_strategy' => Spatie\LaravelData\Support\Creation\ValidationStrategy::Disabled->value,
 
     /*
      * When using an invalid include, exclude, only or except partial, the package will
