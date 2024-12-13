0d3f4ebafc53315d5109b05e2ce44c910d42d25d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-12-14 00:11:58 +0330

    refactor: remove wrong phpdocs

--- app/Containers/AppSection/User/Tasks/FindUserByIdTask.php
+++ app/Containers/AppSection/User/Tasks/FindUserByIdTask.php
@@ -4,7 +4,6 @@ namespace App\Containers\AppSection\User\Tasks;
 
 use App\Containers\AppSection\User\Data\Repositories\UserRepository;
 use App\Containers\AppSection\User\Models\User;
-use App\Ship\Exceptions\NotFoundException;
 use App\Ship\Parents\Tasks\Task as ParentTask;
 
 class FindUserByIdTask extends ParentTask
@@ -14,9 +13,6 @@ class FindUserByIdTask extends ParentTask
     ) {
     }
 
-    /**
-     * @throws NotFoundException
-     */
     public function run(mixed $userId): User
     {
         return $this->repository->getById($userId);
