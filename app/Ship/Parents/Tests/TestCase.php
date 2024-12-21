82a94802a4b9d2b0d6508b5119ef660c5888c0d3
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-12-21 23:11:42 +0330

    feat!(test): remove `faker` instance variable

--- app/Ship/Parents/Tests/TestCase.php
+++ app/Ship/Parents/Tests/TestCase.php
@@ -4,32 +4,13 @@ namespace App\Ship\Parents\Tests;
 
 use Apiato\Core\Abstracts\Tests\TestCase as AbstractTestCase;
 use App\Ship\Enums\AuthGuard;
-use Faker\Generator;
-use Illuminate\Contracts\Console\Kernel as ApiatoConsoleKernel;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Artisan;
-use JetBrains\PhpStorm\Deprecated;
 
 abstract class TestCase extends AbstractTestCase
 {
-    #[Deprecated(reason: 'Laravel already provides a helper function for this', replacement: 'fake(%parameter0%)')]
-    protected Generator $faker;
-
     public static function authGuardDataProvider(): array
     {
         return array_map(static fn (AuthGuard $guard) => [$guard->value], AuthGuard::cases());
-    }
-
-    public function createApplication(): Application
-    {
-        $app = require __DIR__ . '/../../../../bootstrap/app.php';
-
-        $app->make(ApiatoConsoleKernel::class)->bootstrap();
-
-        // create an instance of faker and make it available in all tests
-        $this->faker = $app->make(Generator::class);
-
-        return $app;
     }
 
     protected function afterRefreshingDatabase(): void
