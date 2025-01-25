6f8259d12aaef0aedfba84c545b539d8a7b26cfe
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-25 11:09:35 +0330

    Create RouteServiceProvider.php

---
+++ app/Ship/Parents/Providers/RouteServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Ship\Parents\Providers;
+
+use Apiato\Abstract\Providers\RouteServiceProvider as AbstractRouteServiceProvider;
+
+abstract class RouteServiceProvider extends AbstractRouteServiceProvider
+{
+}
