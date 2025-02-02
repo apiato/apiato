3efad1bf4db54e54d9e308f6bd14817f1ecec39e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-02 18:22:31 +0330

    Update Permission.php

--- app/Containers/AppSection/Authorization/Models/Permission.php
+++ app/Containers/AppSection/Authorization/Models/Permission.php
@@ -2,14 +2,14 @@
 
 namespace App\Containers\AppSection\Authorization\Models;
 
-use Apiato\Foundation\Support\Traits\Model\ModelTrait;
+use Apiato\Abstract\Models\ApiatoIntegration;
 use Apiato\Http\Resources\ResourceKeyAware;
 use App\Containers\AppSection\Authorization\Data\Collections\PermissionCollection;
 use Spatie\Permission\Models\Permission as SpatiePermission;
 
 class Permission extends SpatiePermission implements ResourceKeyAware
 {
-    use ModelTrait;
+    use ApiatoIntegration;
 
     protected $fillable = [
         'name',
