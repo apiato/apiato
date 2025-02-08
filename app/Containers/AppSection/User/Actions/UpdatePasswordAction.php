7eff6db0ad14674430bec534bf0d6ea9f6186a7e
Mohammad-Alavi <Mohammad-Alavi@users.noreply.github.com>
2025-02-08 13:17:15 +0000

    style: apply automated php-cs-fixer changes

--- app/Containers/AppSection/User/Actions/UpdatePasswordAction.php
+++ app/Containers/AppSection/User/Actions/UpdatePasswordAction.php
@@ -5,7 +5,6 @@ namespace App\Containers\AppSection\User\Actions;
 use App\Containers\AppSection\User\Models\User;
 use App\Containers\AppSection\User\Notifications\PasswordUpdatedNotification;
 use App\Containers\AppSection\User\Tasks\UpdateUserTask;
-use App\Containers\AppSection\User\UI\API\Requests\UpdatePasswordRequest;
 use App\Ship\Exceptions\ResourceNotFound;
 use App\Ship\Parents\Actions\Action as ParentAction;
 use Prettus\Validator\Exceptions\ValidatorException;
