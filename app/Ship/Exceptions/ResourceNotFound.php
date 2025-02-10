23f7d00f8c850831d4100393c87fd7d2cec9f958
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-10 20:19:02 +0330

    Update ResourceNotFound.php

--- app/Ship/Exceptions/ResourceNotFound.php
+++ app/Ship/Exceptions/ResourceNotFound.php
@@ -5,10 +5,10 @@ namespace App\Ship\Exceptions;
 use App\Ship\Parents\Exceptions\HttpException as ParentHttpException;
 use Symfony\Component\HttpFoundation\Response;
 
-class ResourceNotFound extends ParentHttpException
+final class ResourceNotFound extends ParentHttpException
 {
     public static function create(string $resourceName = 'Resource'): static
     {
-        return new static(Response::HTTP_NOT_FOUND, "{$resourceName} not found.");
+        return new self(Response::HTTP_NOT_FOUND, "{$resourceName} not found.");
     }
 }
