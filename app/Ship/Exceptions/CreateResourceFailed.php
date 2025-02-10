d5c2e28e71fa31093d61d7cca1fc88980731d332
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-10 20:19:00 +0330

    Update CreateResourceFailed.php

--- app/Ship/Exceptions/CreateResourceFailed.php
+++ app/Ship/Exceptions/CreateResourceFailed.php
@@ -5,10 +5,10 @@ namespace App\Ship\Exceptions;
 use App\Ship\Parents\Exceptions\HttpException as ParentHttpException;
 use Symfony\Component\HttpFoundation\Response;
 
-class CreateResourceFailed extends ParentHttpException
+final class CreateResourceFailed extends ParentHttpException
 {
     public static function create(string $resourceName = 'Resource'): static
     {
-        return new static(Response::HTTP_EXPECTATION_FAILED, "{$resourceName} creation failed.");
+        return new self(Response::HTTP_EXPECTATION_FAILED, "{$resourceName} creation failed.");
     }
 }
