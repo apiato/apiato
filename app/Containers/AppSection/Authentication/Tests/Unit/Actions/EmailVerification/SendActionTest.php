72d584aa77ab84b081ae7bcce7ed884706b82d2b
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-21 16:05:19 +0330

    Update SendActionTest.php

--- app/Containers/AppSection/Authentication/Tests/Unit/Actions/EmailVerification/SendActionTest.php
+++ app/Containers/AppSection/Authentication/Tests/Unit/Actions/EmailVerification/SendActionTest.php
@@ -5,29 +5,39 @@ namespace App\Containers\AppSection\Authentication\Tests\Unit\Actions\EmailVerif
 use App\Containers\AppSection\Authentication\Actions\EmailVerification\SendAction;
 use App\Containers\AppSection\Authentication\Tests\UnitTestCase;
 use App\Containers\AppSection\Authentication\UI\API\Requests\EmailVerification\SendRequest;
+use App\Containers\AppSection\User\Data\Factories\UserFactory;
 use App\Containers\AppSection\User\Models\User;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Support\Facades\Notification;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\TestWith;
 
 #[CoversClass(SendAction::class)]
 class SendActionTest extends UnitTestCase
 {
-    public function testCanSendVerificationEmail(): void
+    #[TestWith([false], 'verified')]
+    #[TestWith([true], 'unverified')]
+    public function testCanSendVerificationEmail(bool $unverified): void
     {
         Notification::fake();
-        $user = User::factory()->unverified()->createOne();
+        $user = tap(
+            User::factory(),
+            static function (UserFactory $factory) use ($unverified) {
+                if ($unverified) {
+                    $factory->unverified();
+                }
+            },
+        )->createOne();
         $action = app(SendAction::class);
         $request = SendRequest::injectData([], $user);
 
         $action->run($request);
 
-        $verificationEnabled = is_a($user, MustVerifyEmail::class);
-        if ($verificationEnabled) {
+        $verificationEnabled = $user instanceof MustVerifyEmail;
+        if ($verificationEnabled && !$user->hasVerifiedEmail()) {
             Notification::assertSentTo($user, VerifyEmail::class);
-        }
-        if (!$verificationEnabled) {
+        } else {
             Notification::assertNotSentTo($user, VerifyEmail::class);
         }
     }
