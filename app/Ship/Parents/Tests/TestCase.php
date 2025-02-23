60e7807f7479b09894d9db92cb5a8af0a4460196
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-23 15:31:33 +0330

    Update TestCase.php

--- app/Ship/Parents/Tests/TestCase.php
+++ app/Ship/Parents/Tests/TestCase.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Artisan;
 
 // TODO:
 //  Is there a better way to install passport?
-//  Do we need to do it?
-//  Where we need it?
 abstract class TestCase extends AbstractTestCase
 {
     use LazilyRefreshDatabase;
