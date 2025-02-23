fe7ba7990e46edd1af8848a1d85cf933c929b2cd
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-23 14:29:18 +0330

    refactor: simplify

--- app/Containers/AppSection/User/UI/API/Controllers/GetUserProfileController.php
+++ app/Containers/AppSection/User/UI/API/Controllers/GetUserProfileController.php
@@ -2,16 +2,14 @@
 
 namespace App\Containers\AppSection\User\UI\API\Controllers;
 
-use App\Containers\AppSection\User\Actions\GetUserProfileAction;
 use App\Containers\AppSection\User\UI\API\Transformers\UserTransformer;
 use App\Ship\Parents\Controllers\ApiController;
+use Illuminate\Support\Facades\Auth;
 
 final class GetUserProfileController extends ApiController
 {
-    public function __invoke(GetUserProfileAction $action): array
+    public function __invoke(): array
     {
-        $user = $action->run();
-
-        return $this->transform($user, UserTransformer::class);
+        return $this->transform(Auth::user(), UserTransformer::class);
     }
 }
