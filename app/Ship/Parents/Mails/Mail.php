d171795c7a2c94dddeceaf75c93bb534ca2d656c
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-28 20:24:18 +0330

    Update Mail.php

--- app/Ship/Parents/Mails/Mail.php
+++ app/Ship/Parents/Mails/Mail.php
@@ -3,7 +3,10 @@
 namespace App\Ship\Parents\Mails;
 
 use Apiato\Abstract\Mails\Mail as AbstractMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
 
 abstract class Mail extends AbstractMail
 {
+    use Queueable, SerializesModels;
 }
