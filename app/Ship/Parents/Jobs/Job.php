2b6e4aecede999409f66af1a21da4450083e5c6e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-28 20:24:13 +0330

    Update Job.php

--- app/Ship/Parents/Jobs/Job.php
+++ app/Ship/Parents/Jobs/Job.php
@@ -3,7 +3,10 @@
 namespace App\Ship\Parents\Jobs;
 
 use Apiato\Abstract\Jobs\Job as AbstractJob;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
 
-abstract class Job extends AbstractJob
+abstract class Job extends AbstractJob implements ShouldQueue
 {
+    use Queueable;
 }
