a792992c4068b052151fb8425acc4c82eadc26d8
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-28 15:48:15 +0330

    refactor: sync root files with Laravel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            // TODO
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
