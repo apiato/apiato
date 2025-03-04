26da7a1f1eb4b0f91d1264df6232837910081b3b
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-03-04 18:29:27 +0330

    Update Value.php

--- app/Ship/Parents/Values/Value.php
+++ app/Ship/Parents/Values/Value.php
@@ -3,9 +3,7 @@
 namespace App\Ship\Parents\Values;
 
 use Apiato\Core\Values\Value as AbstractValue;
-use Illuminate\Contracts\Support\Arrayable;
 
-abstract readonly class Value extends AbstractValue implements Arrayable
+abstract readonly class Value extends AbstractValue
 {
-    abstract public function toArray(): array;
 }
