b4376858f921c3aab971d47bca2904621082d3a0
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-03-07 14:56:41 +0330

    Update Client.php

--- app/Containers/AppSection/Authentication/Values/Clients/Client.php
+++ app/Containers/AppSection/Authentication/Values/Clients/Client.php
@@ -5,6 +5,8 @@ namespace App\Containers\AppSection\Authentication\Values\Clients;
 interface Client
 {
     public function id(): mixed;
+
     public function secret(): string;
+
     public function instance(): \Laravel\Passport\Client;
 }
