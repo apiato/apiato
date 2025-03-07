5df93a3b385e3b6344ef34576bcc8b4c6ddb19eb
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-03-07 14:56:28 +0330

    Update User.php

--- app/Containers/AppSection/User/Models/User.php
+++ app/Containers/AppSection/User/Models/User.php
@@ -2,17 +2,12 @@
 
 namespace App\Containers\AppSection\User\Models;
 
-use App\Containers\AppSection\Authentication\Data\DTOs\Token;
-use App\Containers\AppSection\Authentication\Exceptions\LoginFailed;
-use App\Containers\AppSection\Authentication\Values\OAuth2\Proxies\PasswordGrant\PasswordGrantProxy;
 use App\Containers\AppSection\Authorization\Enums\Role as RoleEnum;
 use App\Containers\AppSection\User\Data\Collections\UserCollection;
 use App\Containers\AppSection\User\Enums\Gender;
 use App\Ship\Parents\Models\UserModel as ParentUserModel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Request;
 
 final class User extends ParentUserModel implements MustVerifyEmail
 {
@@ -35,25 +30,6 @@ final class User extends ParentUserModel implements MustVerifyEmail
         'gender' => Gender::class,
         'birth' => 'immutable_date',
     ];
-
-    public static function issueToken(PasswordGrantProxy $proxy): Token
-    {
-        $authFullApiUrl = route('passport.token');
-        $headers = [
-            'HTTP_ACCEPT' => 'application/json',
-            'HTTP_ACCEPT_LANGUAGE' => request()->headers->get('accept-language', config('app.locale')),
-        ];
-
-        $request = Request::create($authFullApiUrl, 'POST', $proxy->toArray(), server: $headers);
-        $response = App::handle($request);
-        $content = json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR);
-
-        if (!$response->isOk()) {
-            throw LoginFailed::create($content['message']);
-        }
-
-        return Token::fromArray($content);
-    }
 
     public function newCollection(array $models = []): UserCollection
     {
