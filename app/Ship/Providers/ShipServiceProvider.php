33a23cd418f1af9535047cd9b55c3011a85f2c0e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-03-15 15:18:55 +0330

    Update ShipServiceProvider.php

--- app/Ship/Providers/ShipServiceProvider.php
+++ app/Ship/Providers/ShipServiceProvider.php
@@ -3,7 +3,12 @@
 namespace App\Ship\Providers;
 
 use App\Ship\Parents\Providers\ServiceProvider as ParentServiceProvider;
+use Illuminate\Http\Client\RequestException;
 
 class ShipServiceProvider extends ParentServiceProvider
 {
+    public function boot(): void
+    {
+        RequestException::dontTruncate();
+    }
 }
