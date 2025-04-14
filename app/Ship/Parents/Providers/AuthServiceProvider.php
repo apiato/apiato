cd217deb8530dd7817913e8daff5fac5d745d151
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-04-14 16:45:11 +0330

    Delete AuthServiceProvider.php

--- app/Ship/Parents/Providers/AuthServiceProvider.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Ship\Parents\Providers;
-
-use Apiato\Core\Providers\AuthServiceProvider as AbstractAuthServiceProvider;
-
-abstract class AuthServiceProvider extends AbstractAuthServiceProvider
-{
-}
